ccc9e1ef5fbfd390b18c28386f2600f7aa02fa3c
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2019-07-03 15:58:12 +0200

    deps: add and configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'extends': 'standard',
+  'rules': {
+    'arrow-parens': ['error', 'always'],
+    'brace-style': ['error', 'stroustrup'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'line-comment-position': ['error', { 'position': 'above' }],
+    'padded-blocks': 'off',
+    'semi': ['error', 'always'],
+    'operator-linebreak': ['error', 'before'],
+  },
+};
--- stories/hello.stories.js
+++ stories/hello.stories.js
@@ -1,8 +1,8 @@
-'use strict'
+'use strict';
 
-import { storiesOf } from '@storybook/html'
+import { storiesOf } from '@storybook/html';
 
 storiesOf('Hello component', module)
   .add('Hello story', () => {
-    return `<h1>Hello</h1>`
-  }, { notes: 'Hello docs' })
+    return `<h1>Hello</h1>`;
+  }, { notes: 'Hello docs' });
