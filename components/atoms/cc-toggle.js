ad4303b6490bd045c7b2047dbb5ae63fc8957c1c
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2019-07-03 16:06:00 +0200

    cc-toggle: init component

---
+++ components/atoms/cc-toggle.js
@@ -0,0 +1,126 @@
+import { css, html, LitElement } from 'lit-element';
+import { classMap } from 'lit-html/directives/class-map.js';
+import { dispatchCustomEvent } from '../lib/events.js';
+import { repeat } from 'lit-html/directives/repeat.js';
+
+/**
+ * Single choice toggle (a better looking radio input group)
+ *
+ * @fires {String} cc-toggle:input - the selected/toggled value
+ *
+ * @attr {String} value - the selected value
+ * @attr {Boolean} disabled - disables the whole radio input group
+ * @attr {Array} choices - the list of choices
+ */
+export class CcToggle extends LitElement {
+
+  static get properties () {
+    return {
+      value: { type: String },
+      disabled: { type: Boolean },
+      choices: { type: Array, attribute: false },
+    };
+  }
+
+  _onChange (e) {
+    this.value = e.target.value;
+    dispatchCustomEvent(this, 'input', this.value);
+  }
+
+  render () {
+    return html`
+      <div class="${classMap({ 'toggle-group': true, disabled: this.disabled, enabled: !this.disabled })}">
+        ${repeat(this.choices, ({ value }) => value, ({ label, value }) => html`
+          <input
+            type="radio"
+            name="mode"
+            value="${value}"
+            id="${value}"
+            ?disabled="${this.disabled}"
+            ?checked="${this.value === value}"
+            @change="${this._onChange}">
+          <label for="${value}">${label}</label>
+          `)}
+      </div>
+    `;
+  }
+
+  static get styles () {
+    return [
+      // language=CSS
+      css`
+        :host {
+          --toggle-color: hsl(210, 23%, 26%);
+          display: flex;
+          margin: 0.2rem;
+        }
+
+        .toggle-group {
+          border-radius: 0.15rem;
+          border: 1px solid var(--toggle-color);
+          display: flex;
+          overflow: hidden;
+        }
+
+        .toggle-group.enabled:focus-within {
+          box-shadow: 0 0 0 .2em rgba(50, 115, 220, .25);
+          outline: 0;
+        }
+
+        .toggle-group.enabled:hover {
+          box-shadow: 0 1px 3px #888;
+        }
+
+        .toggle-group.enabled:active {
+          box-shadow: none;
+          outline: 0;
+        }
+
+        .toggle-group.disabled {
+          opacity: .5;
+        }
+
+        input {
+          -moz-appearance: none;
+          -webkit-appearance: none;
+          appearance: none;
+          border: 0;
+          display: block;
+          height: 0;
+          margin: 0;
+          outline: none;
+          width: 0;
+        }
+
+        label {
+          -moz-user-select: none;
+          -webkit-user-select: none;
+          -ms-user-select: none;
+          user-select: none;
+          cursor: pointer;
+          text-transform: uppercase;
+          font-weight: bold;
+          font-size: 14px;
+          color: var(--toggle-color);
+          padding: 0.35rem 0.5rem;
+        }
+
+        .disabled label {
+          cursor: default;
+        }
+
+        input:not(:checked):enabled:hover + label {
+          background-color: hsl(210, 23%, 95%);
+        }
+
+        input:checked + label {
+          background-color: var(--toggle-color);
+          color: white;
+          position: relative;
+        }
+      `,
+    ];
+  }
+}
+
+window.customElements.define('cc-toggle', CcToggle);
---
+++ stories/atoms/cc-toggle.stories.js
@@ -0,0 +1,41 @@
+import '../../components/atoms/cc-toggle.js';
+import notes from '../../.components-docs/cc-toggle.md';
+import { storiesOf } from '@storybook/html';
+import { withCustomEventActions } from '../lib/event-action';
+
+const withActions = withCustomEventActions('cc-toggle:input');
+
+storiesOf('atoms', module)
+  .add('<cc-toggle>', withActions(() => {
+
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = `
+      <div class="title">Multiple choices:</div>
+      <cc-toggle id="ONE" value="PAUL"></cc-toggle>
+      
+      <div class="title">Classic 2 choices (true/false):</div>
+      <cc-toggle id="TWO" value="true"></cc-toggle>
+      
+      <div class="title">Disabled:</div>
+      <cc-toggle id="THREE" value="true" disabled></cc-toggle>
+    `;
+
+    wrapper.querySelector('#ONE').choices = [
+      { label: 'John', value: 'JOHN' },
+      { label: 'Paul', value: 'PAUL' },
+      { label: 'George', value: 'GEORGE' },
+      { label: 'Ringo', value: 'RINGO' },
+    ];
+
+    wrapper.querySelector('#TWO').choices = [
+      { label: 'true', value: 'true' },
+      { label: 'false', value: 'false' },
+    ];
+
+    wrapper.querySelector('#THREE').choices = [
+      { label: 'true', value: 'true' },
+      { label: 'false', value: 'false' },
+    ];
+
+    return wrapper;
+  }), { notes });
