9434f18ca4aa9aceafad4c7d236a9fe7d512bf66
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2019-10-15 12:01:18 +0200

    Add storybook addon a11y

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import '@storybook/addon-actions/register'
-import '@storybook/addon-knobs/register'
-import '@storybook/addon-notes/register'
+import '@storybook/addon-a11y/register';
+import '@storybook/addon-actions/register';
+import '@storybook/addon-knobs/register';
+import '@storybook/addon-notes/register';
