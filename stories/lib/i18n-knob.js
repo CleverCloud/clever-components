45141fe49da4b0bb00bfdb43325d134f6fe99576
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2019-10-16 17:54:26 +0200

    Add keyboard shortcut to toggle i18n between en/fr (i)

--- stories/lib/i18n-knob.js
+++ stories/lib/i18n-knob.js
@@ -1,7 +1,8 @@
 import * as en from '../../components/translations/translations.en.js';
 import * as fr from '../../components/translations/translations.fr.js';
 import { addTranslations, getAvailableLanguages, getLanguage, setLanguage } from '../../components/lib/i18n.js';
-import { select } from '@storybook/addon-knobs';
+import { color, select } from '@storybook/addon-knobs';
+import { forceReRender } from '@storybook/html';
 
 // NOTE: this project could be interesting but it's not ready (no npm package and install via github fails)
 // https://github.com/CodeByAlex/storybook-i18n-addon
@@ -10,17 +11,37 @@ import { select } from '@storybook/addon-knobs';
 addTranslations(en.lang, en.translations);
 addTranslations(fr.lang, fr.translations);
 
-const INIT_LANG = localStorage.getItem('I18N_LANG') || 'en';
+const INIT_LANG = window.localStorage.getItem('I18N_LANG') || 'en';
 
 // Default to English
 setLanguage(INIT_LANG);
 
+let keyboardShortcutWasPressed = false;
+window.addEventListener('keypress', ({ keyCode, altKey, ctrlKey, metaKey, shiftKey }) => {
+  // "i" key
+  if (keyCode === 105 && !altKey && !ctrlKey && !metaKey && !shiftKey) {
+    keyboardShortcutWasPressed = true;
+    (getLanguage() === 'en')
+      ? setLanguage('fr')
+      : setLanguage('en');
+    forceReRender();
+  }
+});
+
 export function i18nKnob () {
+
+  if (keyboardShortcutWasPressed) {
+    // This is very very ugly, but this trick helps to reset the knob default value
+    // Maybe the knob will be updated automatically after 5.3
+    // https://github.com/storybookjs/storybook/issues/8376
+    color('Language');
+  }
 
   const i18nValue = select('Language', getAvailableLanguages(), getLanguage());
 
-  if (getLanguage() !== i18nValue) {
+  if (getLanguage() !== i18nValue && !keyboardShortcutWasPressed) {
     setLanguage(i18nValue);
-    localStorage.setItem('I18N_LANG', i18nValue);
   }
+  window.localStorage.setItem('I18N_LANG', getLanguage());
+  keyboardShortcutWasPressed = false;
 }
