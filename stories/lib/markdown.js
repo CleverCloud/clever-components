1c65236a92449a0faf1db8be7bd0616dba571e9b
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2019-10-17 17:05:35 +0200

    Render ADR documents in storybook

---
+++ stories/lib/markdown.js
@@ -0,0 +1,60 @@
+import 'github-markdown-css/github-markdown.css';
+import 'highlight.js/styles/vs.css';
+import hljs from 'highlight.js';
+import MarkdownIt from 'markdown-it';
+
+const md = new MarkdownIt({
+  // Enable HTML tags in source
+  html: true,
+  // Use '/' to close single tags (<br />).
+  xhtmlOut: false,
+  // This is only for full CommonMark compatibility.
+  // Convert '\n' in paragraphs into <br>
+  breaks: false,
+  // CSS language prefix for fenced blocks. Can be useful for external highlighters.
+  langPrefix: 'language-',
+
+  // Autoconvert URL-like text to links
+  linkify: true,
+
+  // Enable some language-neutral replacement + quotes beautification
+  typographer: true,
+
+  // Double + single quotes replacement pairs, when typographer enabled,
+  // and smartquotes on. Could be either a String or an Array.
+  // For example, you can use '«»„“' for Russian, '„“‚‘' for German,
+  // and ['«\xA0', '\xA0»', '‹\xA0', '\xA0›'] for French (including nbsp).
+  quotes: '“”‘’',
+
+  // Highlighter function. Should return escaped HTML,
+  // or '' if the source string is not changed and should be escaped externally.
+  // If result starts with <pre... internal wrapper is skipped.
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      }
+      catch (__) {
+      }
+    }
+
+    // use external default escaping
+    return '';
+  },
+});
+
+export function markdownToDom (markdownText) {
+
+  const html = md.render(markdownText);
+
+  const element = document.createElement('div');
+  element.style.width = '100%';
+  element.style.maxWidth = '55rem';
+  element.style.margin = '5rem auto';
+  element.classList.add('markdown-body');
+  element.innerHTML = html;
+
+  const title = element.querySelector('h1').textContent;
+
+  return { title, element };
+}
--- stories/welcome.stories.js
+++ stories/welcome.stories.js
@@ -1,12 +1,22 @@
 import changelog from '../CHANGELOG.md';
 import contributing from '../CONTRIBUTING.md';
-import marked from 'marked';
 import readme from '../README.md';
 import webComponents from '../WEB-COMPONENTS.md';
+import { markdownToDom } from './lib/markdown.js';
 import { storiesOf } from '@storybook/html';
 
+const adrReq = require.context('../docs', true, /adr.+md$/);
+
 storiesOf('0. Welcome|Documentation', module)
-  .add('Readme', () => marked(readme))
-  .add('Contributing', () => marked(contributing))
-  .add('Changelog', () => marked(changelog))
-  .add('Web Components', () => marked(webComponents));
+  .add('Readme', () => markdownToDom(readme).element)
+  .add('Contributing', () => markdownToDom(contributing).element)
+  .add('Changelog', () => markdownToDom(changelog).element)
+  .add('Web Components', () => markdownToDom(webComponents).element);
+
+const adrAsStories = storiesOf('0. Welcome|Architecture Decision Records', module);
+
+adrReq.keys().forEach(filename => {
+  const markdownText = adrReq(filename).default;
+  const { title, element } = markdownToDom(markdownText);
+  adrAsStories.add(title, () => element);
+});
