353b638544476765fa4930df8b7bec3a151396c9
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2019-12-10 23:26:20 +0100

    Storybook config: add comments about addon order

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
+// The order of those imports will be the same as the order of tabs in addons pane
 import '@storybook/addon-actions/register';
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-a11y/register';
