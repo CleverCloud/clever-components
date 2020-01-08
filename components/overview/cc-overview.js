308cd6276454285736dcee70b4ee0eb52c4fcf64
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2020-01-09 00:11:49 +0100

    cc-overview: allow multiple heads with `--cc-overview-head-count`

--- components/overview/cc-overview.js
+++ components/overview/cc-overview.js
@@ -2,11 +2,12 @@ import { css, html, LitElement } from 'lit-element';
 import { withResizeObserver } from '../mixins/with-resize-observer.js';
 
 /**
- * A display only component (just HTML+CSS) to layout a header, a main and several tiles.
+ * A display only component (just HTML+CSS) to layout heads, a main and several tiles.
  *
  * ## Details
  *
- * * The header component must have the `head` CSS class, it will be displayed at the top.
+ * * The head components must have the `head` CSS class, they will be displayed at the top.
+ * * If you use more than one `head, you must specify how many with `--cc-overview-head-count`.
  * * The main component must have the `main` CSS class, it will be displayed at the bottom left (depending on the number of columns).
  * * The tile components will be displayed in a 1 to 4 columns grid layout, below the header and around the main.
  * * The number of columns is variable and depends directly on the width of the component (with some help from `withResizeObserver`).
@@ -16,6 +17,8 @@ import { withResizeObserver } from '../mixins/with-resize-observer.js';
  * @prop {"app"|"orga"} mode - Sets the mode of the layout for the overview.
  *
  * @slot - Put your `.head`, tiles and `.main` components here.
+ *
+ * @cssprop {Number} --cc-overview-head-count - How many `.head` elements marked are in the slot  (defaults: `1`).
  */
 export class CcOverview extends withResizeObserver(LitElement) {
 
@@ -47,53 +50,55 @@ export class CcOverview extends withResizeObserver(LitElement) {
       :host {
         display: grid;
         grid-gap: 1rem;
+        /* We ask the user to specify this number (if > 1) because the information is known and detecting it automatically and properly requires a MutationObserver to count them in the \`<slot>\` and it seems overkill. */
+        --cc-overview-head-count: 1;
       }
 
       /* GRID LAYOUT */
       :host([w-lt-570]) {
-        grid-template-columns: repeat(1, 1fr);
+        grid-template-columns: [main-start] 1fr [main-end];
       }
 
       :host([w-gte-570][w-lt-860]) {
-        grid-template-columns: repeat(2, 1fr);
+        grid-template-columns: [main-start] 1fr 1fr [main-end];
       }
 
       :host([w-gte-860][w-lt-1150]) {
-        grid-template-columns: repeat(3, 1fr);
+        grid-template-columns: [main-start] 1fr 1fr [main-end] 1fr;
       }
 
       :host([w-gte-1150]) {
-        grid-template-columns: repeat(4, 1fr);
+        grid-template-columns: [main-start] 1fr 1fr 1fr [main-end] 1fr;
       }
 
       /* GRID LAYOUT (app) */
       :host([mode="app"][w-lt-570]) {
-        grid-template-rows: repeat(7, min-content) 1fr;
+        grid-template-rows: repeat(calc(var(--cc-overview-head-count) + 6), min-content) [main-start] 1fr [main-end];
       }
 
       :host([mode="app"][w-gte-570][w-lt-860]) {
-        grid-template-rows: repeat(4, min-content) 1fr;
+        grid-template-rows: repeat(calc(var(--cc-overview-head-count) + 3), min-content) [main-start] 1fr [main-end];
       }
 
       :host([mode="app"][w-gte-860][w-lt-1150]) {
-        grid-template-rows: repeat(3, min-content) 1fr 1fr;
+        grid-template-rows: repeat(calc(var(--cc-overview-head-count) + 1), min-content) [main-start] min-content 1fr 1fr [main-end];
       }
 
       :host([mode="app"][w-gte-1150]) {
-        grid-template-rows: repeat(2, min-content) 1fr 1fr;
+        grid-template-rows: repeat(calc(var(--cc-overview-head-count) + 1), min-content) [main-start] 1fr 1fr [main-end];
       }
 
       /* GRID LAYOUT (orga) */
       :host([mode="orga"][w-lt-570]) {
-        grid-template-rows: repeat(3, min-content) 1fr;
+        grid-template-rows: repeat(calc(var(--cc-overview-head-count) + 2), min-content) [main-start] 1fr [main-end];
       }
 
       :host([mode="orga"][w-gte-570][w-lt-860]) {
-        grid-template-rows: repeat(2, min-content) 1fr;
+        grid-template-rows: repeat(calc(var(--cc-overview-head-count) + 1), min-content) [main-start] 1fr [main-end];
       }
 
       :host([mode="orga"][w-gte-860]) {
-        grid-template-rows: repeat(1, min-content) 1fr 1fr;
+        grid-template-rows: repeat(calc(var(--cc-overview-head-count) + 0), min-content) [main-start] 1fr 1fr [main-end];
       }
 
       /* .head TILE POSITION/SIZE */
@@ -103,22 +108,11 @@ export class CcOverview extends withResizeObserver(LitElement) {
 
       /* .main TILE POSITION/SIZE */
       ::slotted(*.main) {
-        grid-column: 1 / -1;
+        grid-column: main-start / main-end;
+        grid-row: main-start / main-end;
         height: auto;
-        width: auto;
         min-height: 25rem;
-      }
-
-      :host([w-gte-860]) ::slotted(*.main) {
-        grid-column: 1 / -2;
-      }
-
-      :host([mode="app"][w-gte-860]) ::slotted(*.main) {
-        grid-row: 3 / -1;
-      }
-
-      :host([mode="orga"][w-gte-860]) ::slotted(*.main) {
-        grid-row: 2 / -1;
+        width: auto;
       }
     `;
   }
