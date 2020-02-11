2af1846ae6126d41a8541a5d0990744100b0e25b
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2020-02-11 11:56:57 +0100

    cc-error: remove twemoji dep and store warning icon locally

--- components/molecules/cc-error.js
+++ components/molecules/cc-error.js
@@ -1,6 +1,6 @@
 import '../atoms/cc-button.js';
 import '../atoms/cc-loader.js';
-import warningSvg from 'twemoji/2/svg/26a0.svg';
+import warningSvg from './warning.svg';
 import { css, html, LitElement } from 'lit-element';
 import { dispatchCustomEvent } from '../lib/events.js';
 import { i18n } from '../lib/i18n.js';
