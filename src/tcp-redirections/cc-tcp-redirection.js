bbfeb694ffb796590d442c9b253f91fc2085b91d
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2020-04-23 01:09:21 +0200

    cc-tcp-redirection: fix "typing" error in `this._getIconUrl()`

--- src/tcp-redirections/cc-tcp-redirection.js
+++ src/tcp-redirections/cc-tcp-redirection.js
@@ -1,7 +1,6 @@
 import '../atoms/cc-button.js';
 import { css, html, LitElement } from 'lit-element';
 import { classMap } from 'lit-html/directives/class-map.js';
-import { ifDefined } from 'lit-html/directives/if-defined.js';
 import { assetUrl } from '../lib/asset-url.js';
 import { dispatchCustomEvent } from '../lib/events.js';
 import { i18n } from '../lib/i18n.js';
@@ -101,9 +100,6 @@ export class CcTcpRedirection extends LitElement {
   }
 
   _getIconUrl () {
-    if (this.skeleton) {
-      return null;
-    }
     if (this.error) {
       return warningSvg;
     }
@@ -128,8 +124,8 @@ export class CcTcpRedirection extends LitElement {
   render () {
     return html`
       <span class="icon ${classMap({ skeleton: this.skeleton })}">
-        ${!this.waiting ? html`
-          <img src=${ifDefined(this._getIconUrl())} alt="">
+        ${!this.waiting && !this.skeleton ? html`
+          <img src=${this._getIconUrl()} alt="">
         ` : ''}
         ${this.waiting ? html`
       <cc-loader></cc-loader>
@@ -180,10 +176,6 @@ export class CcTcpRedirection extends LitElement {
           display: block;
           height: 100%;
           width: 100%;
-        }
-
-        .skeleton img {
-          display: none;
         }
 
         .icon cc-loader {
