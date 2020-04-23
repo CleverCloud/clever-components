b15d5f67b5cb9b1e2f7f6404ca92af8a741692f4
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2020-04-23 15:11:38 +0200

    cc-flex-gap: init component

---
+++ src/atoms/cc-flex-gap.js
@@ -0,0 +1,46 @@
+const createElement = (tagName) => document.createElement(tagName);
+const appendChild = (parent, child) => parent.appendChild(child);
+
+/**
+ * A pure layout component used to overcome the lack of `gap` in CSS flex containers.
+ *
+ * ## Technical details
+ *
+ * * This component does not use lit* deps on purpose (keep small and not useful).
+ * * The source code is written in a way so it can be the smallest possible, while keeping a reasonable readability level.
+ * * This feature exist in Firefox: https://developer.mozilla.org/en-US/docs/Web/CSS/gap.
+ *
+ * Here is the technique:
+ *
+ * * Use `display: flex` on a container.
+ * * Use a negative margin of half the gap on this container.
+ * * Use a positive margin of half the gap on its children.
+ * * Sadly, due to margin collapsing behaviour between the container with negative margins and its parent,
+ * * the negative margin of the container will be displayed as white space in its parent.
+ * * There are a few techniques to prevent this.
+ * * We chose to use a `display: grid` on the parent to disable the margin collapsing while preserving potential overflow like focus rings etc...
+ *
+ * @cssprop {Number} --cc-gap - The gap between children.
+ */
+export class CcFlexGap extends HTMLElement {
+
+  constructor () {
+    super();
+
+    const shadow = this.attachShadow({ mode: 'open' });
+
+    const style = createElement('style');
+    style.textContent = `:host{display:grid}::slotted(*){margin:calc(var(--cc-gap)/2)}`;
+
+    const flexContainer = createElement('div');
+    flexContainer.style = `display:flex;flex-wrap:wrap;margin:calc(var(--cc-gap)/-2);`;
+
+    const slot = createElement('slot');
+
+    appendChild(shadow, style);
+    appendChild(shadow, flexContainer);
+    appendChild(flexContainer, slot);
+  }
+}
+
+window.customElements.define('cc-flex-gap', CcFlexGap);
---
+++ stories/atoms/cc-flex-gap.stories.js
@@ -0,0 +1,53 @@
+import '../../src/atoms/cc-flex-gap.js';
+import notes from '../../.components-docs/cc-flex-gap.md';
+import { makeStory } from '../lib/make-story.js';
+import { enhanceStoriesNames } from '../lib/story-names.js';
+
+export default {
+  title: '🧬 Atoms|<cc-flex-gap>',
+  component: 'cc-flex-gap',
+  parameters: { notes },
+};
+
+const conf = {
+  component: 'cc-flex-gap',
+  css: `
+    cc-flex-gap {
+      background-color: #eee;
+    }
+    
+    cc-flex-gap > * {
+      border-radius: 0.25rem;
+      border:1px solid #000;
+      flex: 1 1 0;
+      padding: 0.5rem 1rem;
+    }
+  `,
+};
+
+const flexItemsHtml = Array.from(new Array(15))
+  .map((a, i) => `<div>item${'_'.repeat(i + 1)}${i + 1}</div>`)
+  .join('');
+
+export const defaultStory = makeStory(conf, {
+  items: [{ innerHTML: flexItemsHtml, style: '--cc-gap: 1rem' }],
+});
+
+export const noGap = makeStory(conf, {
+  items: [{ innerHTML: flexItemsHtml, style: '' }],
+});
+
+export const smallGap = makeStory(conf, {
+  items: [{ innerHTML: flexItemsHtml, style: '--cc-gap: 0.5rem' }],
+});
+
+export const bigGap = makeStory(conf, {
+  items: [{ innerHTML: flexItemsHtml, style: '--cc-gap: 2rem' }],
+});
+
+enhanceStoriesNames({
+  defaultStory,
+  noGap,
+  smallGap,
+  bigGap,
+});
