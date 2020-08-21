2bdc205579ee2e30a1213df9bcfbba2cb6d3b38e
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2020-08-22 00:38:56 +0200

    Refine implementation of prepareNumberUnitFormatter and adapt tests

--- src/lib/i18n-number.js
+++ src/lib/i18n-number.js
@@ -1,23 +1,40 @@
+// Intl.NumberFormat has a `notation: 'compact'` option.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat/NumberFormat
+// This is close to want we want to achieve for compact number display but:
+// * It's not supported on Safari.
+// * In english:
+//   * The prefixes are K, M, B and T with no space between number and prefix (1K, 2M...) (short scale based)
+//   * K for 1 thousand (but why? the SI prefix for 1 kilo is lower case k)
+//     * https://en.wikipedia.org/wiki/1000_(number)
+//     * > Multiples of thousands are occasionally represented by replacing their last three zeros with the letter "K": for instance, writing "$30K" for $30 000, or denoting the Y2K computer bug of the year 2000.
+//   * M for 1 million (not mega)
+//   * B for 1 billion
+//   * T for 1 trillion (not tera)
+// * In french:
+//   * The prefixes are k, M, Md and Bn with a space between number and prefix (1 k, 2 M...) (long scale based)
+//   * k for 1 thousand (similar to SI prefix for 1 kilo)
+//   * M for "un million" (not mega)
+//   * Md for "un milliard"
+//   * Bn for "un billion"
+// For our context, we would prefer no space and just one letter, it's more compact.
+// We also feel like the short/long scale is confusing for the "B" since a billion can be 10^9 or 10^12.
+// Using SI symbols (k, M, G, T...) is simpler to implement and easier maintain.
+// It's also pretty well understood by our kind of technical/scientific users (even in countries that don't use the metric system).
+
+// https://en.wikipedia.org/wiki/Metric_prefix
+const SI_SYMBOLS = ['', 'k', 'M', 'G', 'T', 'P'];
+
 export function prepareNumberUnitFormatter (lang) {
-
-  const numberFormatter = new Intl.NumberFormat(lang, {
-    minimumFractionDigits: 1,
-    maximumFractionDigits: 1,
-  });
-
-  function format (value, suffix) {
-    return (value < 10)
-      ? numberFormatter.format(value) + suffix
-      : Math.round(value) + suffix;
-  }
-
-  return (value) => {
-    if (value >= 1e6) {
-      return format(value / 1e6, 'M');
-    }
-    if (value >= 1e3) {
-      return format(value / 1e3, 'K');
-    }
-    return value;
+  const nf = new Intl.NumberFormat(lang, { minimumFractionDigits: 0, maximumFractionDigits: 1 });
+  return (rawValue) => {
+    // Figure out the "magnitude" of the rawValue: 1000 => 1 / 1000000 => 2 / 1000000000 => 3 ...
+    const symbolIndex = Math.floor(Math.log10(rawValue) / 3);
+    // Use the symbolIndex to "rebase" the rawValue into the new base, 1250 => 1.25 / 1444000 => 1.444...
+    const rebasedValue = rawValue / 1000 ** symbolIndex;
+    // Use Intl/i18n aware number formatter
+    const formattedValue = nf.format(rebasedValue);
+    const symbol = SI_SYMBOLS[symbolIndex];
+    // No space for compact display
+    return formattedValue + symbol;
   };
 }
--- test/i18n-number.test.js
+++ test/i18n-number.test.js
@@ -8,49 +8,49 @@ describe('prepareNumberUnitFormatter', () => {
     const formatNumberUnitEn = prepareNumberUnitFormatter('en');
 
     it('Small values', () => {
-      expect(formatNumberUnitEn(1)).to.equal(1);
-      expect(formatNumberUnitEn(10)).to.equal(10);
-      expect(formatNumberUnitEn(100)).to.equal(100);
+      expect(formatNumberUnitEn(1)).to.equal('1');
+      expect(formatNumberUnitEn(10)).to.equal('10');
+      expect(formatNumberUnitEn(100)).to.equal('100');
     });
 
     it('K values', () => {
-      expect(formatNumberUnitEn(10e2)).to.equal('1.0K');
-      expect(formatNumberUnitEn(10e3)).to.equal('10K');
-      expect(formatNumberUnitEn(10e4)).to.equal('100K');
+      expect(formatNumberUnitEn(10e2)).to.equal('1k');
+      expect(formatNumberUnitEn(10e3)).to.equal('10k');
+      expect(formatNumberUnitEn(10e4)).to.equal('100k');
     });
 
     it('M values', () => {
-      expect(formatNumberUnitEn(10e5)).to.equal('1.0M');
+      expect(formatNumberUnitEn(10e5)).to.equal('1M');
       expect(formatNumberUnitEn(10e6)).to.equal('10M');
       expect(formatNumberUnitEn(10e7)).to.equal('100M');
     });
 
     it('G values', () => {
-      expect(formatNumberUnitEn(10e8)).to.equal('1000M');
-      expect(formatNumberUnitEn(10e9)).to.equal('10000M');
-      expect(formatNumberUnitEn(10e10)).to.equal('100000M');
+      expect(formatNumberUnitEn(10e8)).to.equal('1G');
+      expect(formatNumberUnitEn(10e9)).to.equal('10G');
+      expect(formatNumberUnitEn(10e10)).to.equal('100G');
     });
 
     it('T values', () => {
-      expect(formatNumberUnitEn(10e11)).to.equal('1000000M');
-      expect(formatNumberUnitEn(10e12)).to.equal('10000000M');
-      expect(formatNumberUnitEn(10e13)).to.equal('100000000M');
+      expect(formatNumberUnitEn(10e11)).to.equal('1T');
+      expect(formatNumberUnitEn(10e12)).to.equal('10T');
+      expect(formatNumberUnitEn(10e13)).to.equal('100T');
     });
 
     it('P values', () => {
-      expect(formatNumberUnitEn(10e14)).to.equal('1000000000M');
-      expect(formatNumberUnitEn(10e15)).to.equal('10000000000M');
-      expect(formatNumberUnitEn(10e16)).to.equal('100000000000M');
+      expect(formatNumberUnitEn(10e14)).to.equal('1P');
+      expect(formatNumberUnitEn(10e15)).to.equal('10P');
+      expect(formatNumberUnitEn(10e16)).to.equal('100P');
     });
 
     it('decimal values', () => {
-      expect(formatNumberUnitEn(999)).to.equal(999);
-      expect(formatNumberUnitEn(1049)).to.equal('1.0K');
-      expect(formatNumberUnitEn(1050)).to.equal('1.1K');
+      expect(formatNumberUnitEn(999)).to.equal('999');
+      expect(formatNumberUnitEn(1049)).to.equal('1k');
+      expect(formatNumberUnitEn(1050)).to.equal('1.1k');
       expect(formatNumberUnitEn(2149999)).to.equal('2.1M');
       expect(formatNumberUnitEn(2150000)).to.equal('2.2M');
-      expect(formatNumberUnitEn(3249999999)).to.equal('3250M');
-      expect(formatNumberUnitEn(3250000000)).to.equal('3250M');
+      expect(formatNumberUnitEn(3249999999)).to.equal('3.2G');
+      expect(formatNumberUnitEn(3250000000)).to.equal('3.3G');
     });
   });
 
@@ -59,49 +59,49 @@ describe('prepareNumberUnitFormatter', () => {
     const formatNumberUnitFr = prepareNumberUnitFormatter('fr');
 
     it('Small values', () => {
-      expect(formatNumberUnitFr(1)).to.equal(1);
-      expect(formatNumberUnitFr(10)).to.equal(10);
-      expect(formatNumberUnitFr(100)).to.equal(100);
+      expect(formatNumberUnitFr(1)).to.equal('1');
+      expect(formatNumberUnitFr(10)).to.equal('10');
+      expect(formatNumberUnitFr(100)).to.equal('100');
     });
 
     it('K values', () => {
-      expect(formatNumberUnitFr(10e2)).to.equal('1,0K');
-      expect(formatNumberUnitFr(10e3)).to.equal('10K');
-      expect(formatNumberUnitFr(10e4)).to.equal('100K');
+      expect(formatNumberUnitFr(10e2)).to.equal('1k');
+      expect(formatNumberUnitFr(10e3)).to.equal('10k');
+      expect(formatNumberUnitFr(10e4)).to.equal('100k');
     });
 
     it('M values', () => {
-      expect(formatNumberUnitFr(10e5)).to.equal('1,0M');
+      expect(formatNumberUnitFr(10e5)).to.equal('1M');
       expect(formatNumberUnitFr(10e6)).to.equal('10M');
       expect(formatNumberUnitFr(10e7)).to.equal('100M');
     });
 
     it('G values', () => {
-      expect(formatNumberUnitFr(10e8)).to.equal('1000M');
-      expect(formatNumberUnitFr(10e9)).to.equal('10000M');
-      expect(formatNumberUnitFr(10e10)).to.equal('100000M');
+      expect(formatNumberUnitFr(10e8)).to.equal('1G');
+      expect(formatNumberUnitFr(10e9)).to.equal('10G');
+      expect(formatNumberUnitFr(10e10)).to.equal('100G');
     });
 
     it('T values', () => {
-      expect(formatNumberUnitFr(10e11)).to.equal('1000000M');
-      expect(formatNumberUnitFr(10e12)).to.equal('10000000M');
-      expect(formatNumberUnitFr(10e13)).to.equal('100000000M');
+      expect(formatNumberUnitFr(10e11)).to.equal('1T');
+      expect(formatNumberUnitFr(10e12)).to.equal('10T');
+      expect(formatNumberUnitFr(10e13)).to.equal('100T');
     });
 
     it('P values', () => {
-      expect(formatNumberUnitFr(10e14)).to.equal('1000000000M');
-      expect(formatNumberUnitFr(10e15)).to.equal('10000000000M');
-      expect(formatNumberUnitFr(10e16)).to.equal('100000000000M');
+      expect(formatNumberUnitFr(10e14)).to.equal('1P');
+      expect(formatNumberUnitFr(10e15)).to.equal('10P');
+      expect(formatNumberUnitFr(10e16)).to.equal('100P');
     });
 
     it('decimal values', () => {
-      expect(formatNumberUnitFr(999)).to.equal(999);
-      expect(formatNumberUnitFr(1049)).to.equal('1,0K');
-      expect(formatNumberUnitFr(1050)).to.equal('1,1K');
+      expect(formatNumberUnitFr(999)).to.equal('999');
+      expect(formatNumberUnitFr(1049)).to.equal('1k');
+      expect(formatNumberUnitFr(1050)).to.equal('1,1k');
       expect(formatNumberUnitFr(2149999)).to.equal('2,1M');
       expect(formatNumberUnitFr(2150000)).to.equal('2,2M');
-      expect(formatNumberUnitFr(3249999999)).to.equal('3250M');
-      expect(formatNumberUnitFr(3250000000)).to.equal('3250M');
+      expect(formatNumberUnitFr(3249999999)).to.equal('3,2G');
+      expect(formatNumberUnitFr(3250000000)).to.equal('3,3G');
     });
   });
 });
