4018ce0d13cd2e47ac7730da9cf07957809cce7d
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2020-08-29 17:36:42 +0200

    cc-map-marker-server: init component

---
+++ stories/maps/cc-map-marker-server.stories.js
@@ -0,0 +1,46 @@
+import '../../src/maps/cc-map-marker-server.js';
+import { makeStory } from '../lib/make-story.js';
+import { enhanceStoriesNames } from '../lib/story-names.js';
+
+export default {
+  title: '🛠 Maps/<cc-map-marker-server>',
+  component: 'cc-map-marker-server',
+};
+
+const conf = {
+  component: 'cc-map-marker-server',
+  // language=CSS
+  css: `
+    cc-map-marker-server {
+      display: inline-block;
+      margin: 1rem;
+    }
+  `,
+};
+
+export const defaultStory = makeStory(conf, {
+  items: [
+    { state: 'default' },
+    { state: 'hovered' },
+    { state: 'selected' },
+  ],
+});
+
+export const stateWithDefault = makeStory(conf, {
+  items: [{ state: 'default' }],
+});
+
+export const stateWithHovered = makeStory(conf, {
+  items: [{ state: 'hovered' }],
+});
+
+export const stateWithSelected = makeStory(conf, {
+  items: [{ state: 'selected' }],
+});
+
+enhanceStoriesNames({
+  defaultStory,
+  stateWithDefault,
+  stateWithHovered,
+  stateWithSelected,
+});
