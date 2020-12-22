80af553dbccddeec7aff6219a99bd7eda78c4723
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2020-12-22 12:22:18 +0100

    Replace i18n-knob with i18n-control

--- stories/lib/i18n-control.js
+++ stories/lib/i18n-control.js
@@ -1,5 +1,3 @@
-import { CHANGE, select } from '@storybook/addon-knobs';
-import { addons } from '@storybook/addons';
 import { addTranslations, getAvailableLanguages, getLanguage, setLanguage } from '../../src/lib/i18n.js';
 import * as en from '../../src/translations/translations.en.js';
 import * as fr from '../../src/translations/translations.fr.js';
@@ -16,24 +14,30 @@ setLanguage(INIT_LANG);
 
 const ALL_LANGS = Object.values(getAvailableLanguages());
 
-const channel = addons.getChannel();
+// React hooks voodoo shit
+let updateArgs = () => {
+};
+
+export function setUpdateArgsCallback (callback) {
+  updateArgs = callback;
+}
 
 window.addEventListener('keypress', ({ keyCode, altKey, ctrlKey, metaKey, shiftKey }) => {
   // "i" key
   if (keyCode === 105 && !altKey && !ctrlKey && !metaKey && !shiftKey) {
     const langIdx = ALL_LANGS.indexOf(getLanguage());
     const nextIdx = (langIdx + 1) % ALL_LANGS.length;
-    channel.emit(CHANGE, {
-      name: 'Language',
-      value: ALL_LANGS[nextIdx],
-    });
+    const nextLang = ALL_LANGS[nextIdx];
+    // https://github.com/storybookjs/storybook/issues/3855#issuecomment-660158622
+    updateArgs({ lang: nextLang });
   }
 });
 
-export function i18nKnob () {
-  const i18nValue = select('Language', getAvailableLanguages(), getLanguage());
-  if (getLanguage() !== i18nValue) {
-    setLanguage(i18nValue);
-  }
-  window.localStorage.setItem('I18N_LANG', getLanguage());
+export function getLangArgType () {
+  return { control: { type: 'inline-radio', options: getAvailableLanguages() } };
+}
+
+export function updateLang (lang) {
+  window.localStorage.setItem('I18N_LANG', lang);
+  setLanguage(lang);
 }
