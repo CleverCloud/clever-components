2d984fbe77a04c67ef9e60ccf35414933e31b617
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2022-07-30 01:45:41 +0200

    refactor: adapt CDN and npm rollup builds

--- rollup/rollup-npm.config.js
+++ rollup/rollup-npm.config.js
@@ -4,8 +4,9 @@ import copy from 'rollup-plugin-copy';
 import {
   babelPlugin,
   clearPlugin,
+  getAllSourceFiles,
+  getMainFiles,
   importMetaUrlAssetsPlugin,
-  inputs,
   minifyStylesheet,
   terserPlugin,
   visualizerPlugin,
@@ -14,20 +15,34 @@ import {
 const sourceDir = 'src';
 const outputDir = 'dist';
 
+
+const allSourceFiles = getAllSourceFiles(sourceDir);
+const mainFiles = getMainFiles(sourceDir);
+
+const inputFilesPairs = allSourceFiles.map((sourceDir, (file) => {
+
+  const isMainFile = mainFiles.includes(file);
+  const { dir, name } = path.parse(file);
+
+  // main files go to the root
+  // other files are kept in the same path
+  const entryPath = isMainFile
+    ? name
+    : path.relative(sourceDir, path.join(dir, name));
+
+  return [entryPath, file];
+}));
+
 export default {
   // defines the files to be included into the build: all components + smart manager + translations
-  input: inputs(sourceDir, (file) => {
-    const { name: entryPath } = path.parse(file);
-    return [entryPath, file];
-  }),
+  input: Object.fromEntries(inputFilesPairs),
   output: {
     dir: outputDir,
     sourcemap: true,
     // We don't need the hash in this situation
     assetFileNames: 'assets/[name].[ext]',
+    hoistTransitiveImports: false,
   },
-  // We want to preserve modules
-  preserveModules: true,
   plugins: [
     // output directory cleanup
     clearPlugin({ outputDir }),
@@ -44,6 +59,11 @@ export default {
       targets: [
         {
           src: 'src/styles/default-theme.css',
+          dest: 'dist/styles',
+          transform: (stylesheet) => minifyStylesheet(stylesheet),
+        },
+        {
+          src: 'src/styles/undefined-components.css',
           dest: 'dist/styles',
           transform: (stylesheet) => minifyStylesheet(stylesheet),
         },
