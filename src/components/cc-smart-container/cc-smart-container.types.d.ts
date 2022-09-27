07483d7367ea0ed7bc81d9bdadffef746f8b7d02
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2022-09-27 11:18:37 +0200

    --wip--

--- src/components/cc-smart-container/cc-smart-container.types.d.ts
+++ src/components/cc-smart-container/cc-smart-container.types.d.ts
@@ -1,3 +1,4 @@
 import { SmartComponentDefinition } from '../../lib/define-smart-component.types';
+import { CcSmartContainer } from '../../lib/define-smart-component';
 
 export type SmartComponentDefinitionFoo = SmartComponentDefinition<CcSmartContainer, { foo: boolean, bar: string }>;
