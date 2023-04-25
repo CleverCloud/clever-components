00383442e7385bf1fa16fd9701c157deef982533
Pierre DE SOYRES <pierre.de-soyres@clever-cloud.com>
2023-04-25 15:44:58 +0200

    feat(cc-ansi-palette): init component

---
+++ src/lib/ansi/ansi-palette-analyser.js
@@ -0,0 +1,47 @@
+import { getContrastRatio, hexToRgb, isDark } from '../color.js';
+
+const colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan'];
+
+/**
+ *
+ * @param {AnsiPalette} palette
+ */
+export function analyzePalette (palette) {
+  const background = hexToRgb(palette.background);
+  const colorsToTest = getColorsToTest(isDark(background));
+
+  const colorsCount = colorsToTest.length;
+  let ratioSum = 0;
+  let compliantCount = 0;
+
+  const contrasts = {};
+
+  colorsToTest.forEach((colorName) => {
+    const ratio = getContrastRatio(background, hexToRgb(palette[colorName]));
+    const compliant = ratio >= 4.5;
+    if (compliant) {
+      compliantCount++;
+    }
+    ratioSum += ratio;
+    contrasts[colorName] = { ratio, compliant };
+  });
+
+  return {
+    totalColors: colorsCount,
+    compliantColors: compliantCount,
+    contrastAvg: ratioSum / colorsCount,
+    contrasts,
+  };
+}
+
+function getColorsToTest (isDarkPalette) {
+  const result = [
+    ...colors,
+    isDarkPalette ? 'white' : 'black',
+  ];
+
+  return [
+    ...result,
+    ...result.map((c) => `bright-${c}`),
+  ];
+}
