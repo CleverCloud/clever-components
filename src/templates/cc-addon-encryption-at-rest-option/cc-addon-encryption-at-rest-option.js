137b7e92ad60b7520eef02ad1de4a878a2261876
Galimede <mat.degand@outlook.fr>
2023-06-13 15:32:19 +0200

    feat(cc-addon-encryption-at-rest-option): replace error message to get rid of cc-error

--- src/templates/cc-addon-encryption-at-rest-option/cc-addon-encryption-at-rest-option.js
+++ src/templates/cc-addon-encryption-at-rest-option/cc-addon-encryption-at-rest-option.js
@@ -1,6 +1,9 @@
-import '../../components/cc-error/cc-error.js';
+import '../../components/cc-icon/cc-icon.js';
 import { html } from 'lit';
-import { iconRemixShieldKeyholeFill as iconEncryptionAtRest } from '../../assets/cc-remix.icons.js';
+import {
+  iconRemixAlertFill as iconAlert,
+  iconRemixShieldKeyholeFill as iconEncryptionAtRest,
+} from '../../assets/cc-remix.icons.js';
 import { i18n } from '../../lib/i18n.js';
 
 const PRICE_PERCENT = 0.10;
@@ -8,9 +11,11 @@ const PRICE_PERCENT = 0.10;
 export const ccAddonEncryptionAtRestOption = ({ enabled, price }) => {
   const description = html`
     <div class="option-details">${i18n('cc-addon-encryption-at-rest-option.description')}</div>
-    <cc-error class="option-warning">
-      ${i18n('cc-addon-encryption-at-rest-option.warning', { percent: PRICE_PERCENT, price })}
-    </cc-error>
+    <div class="option-warning">
+        <cc-icon .icon="${iconAlert}" accessible-name="${i18n('cc-addon-encryption-at-rest-option.error.icon-a11y-name')}" class="icon-warning"></cc-icon>
+        <p>${i18n('cc-addon-encryption-at-rest-option.warning', { percent: PRICE_PERCENT, price })}</p>
+    </div>
+    </cc-notice>
   `;
 
   return {
