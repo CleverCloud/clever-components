9cf782e2d555465096931c8d5affd4b1f7926241
Galimede <mat.degand@outlook.fr>
2023-06-13 14:51:35 +0200

    feat(cc-block): get rid of overlay error story

--- src/components/cc-block/cc-block.stories.js
+++ src/components/cc-block/cc-block.stories.js
@@ -39,20 +39,6 @@ export const overlayWithLoader = makeStory(conf, {
   }],
 });
 
-export const overlayWithErrorAlert = makeStory(conf, {
-  items: [{
-    // language=HTML
-    innerHTML: `
-      <div slot="title">This is my block</div>
-      <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque feugiat dui at leo porta dignissim. Etiam ut purus ultrices, pulvinar tellus quis, cursus massa. Mauris dignissim accumsan ex, at vestibulum lectus fermentum id. Quisque nec magna arcu. Quisque in metus sed erat sodales euismod eget id purus. Sed sagittis rhoncus mauris. Ut sit amet urna ac nunc semper porta. Nam ut felis eu velit luctus rutrum. Nam leo nisl, molestie a varius non, ullamcorper sit amet tortor. Donec in convallis ex. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent hendrerit venenatis erat, eu malesuada nulla viverra eu. Curabitur porta risus augue, non rutrum lectus hendrerit a.</div>
-      <div>Sed volutpat dolor nec rutrum vulputate. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer rhoncus turpis orci, at tempor tortor scelerisque varius. Integer nec fermentum dui. Integer vitae dolor sit amet erat ullamcorper elementum. Donec blandit lacinia erat, vitae blandit libero ornare id. In luctus odio a lacus dignissim, id posuere tortor lacinia. Pellentesque sed massa ac tellus tincidunt rutrum. Praesent commodo enim nibh, ut consectetur tortor consequat non. Aliquam mi enim, mattis eu velit quis, sollicitudin fringilla ex. Donec at augue ultrices, porta justo in, mattis tortor. Nunc sollicitudin nisi eget urna condimentum semper. Pellentesque sagittis quam eu mollis viverra. Proin tincidunt auctor nibh quis suscipit.</div>
-      <div slot="overlay">
-        <cc-error mode="info">Something went wrong while loading something really, really important.</cc-error>
-      </div>
-    `,
-  }],
-});
-
 export const image = makeStory(conf, {
   items: [{
     image: 'https://assets.clever-cloud.com/logos/nodejs.svg',
@@ -177,7 +163,6 @@ export const iconAndOpen = makeStory(conf, {
 enhanceStoriesNames({
   defaultStory,
   overlayWithLoader,
-  overlayWithErrorAlert,
   image,
   icon,
   imageAndIcon,
