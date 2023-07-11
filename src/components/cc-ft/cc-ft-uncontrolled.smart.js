82a44c7e7c665cb9a57ceb4ed8208dfd6d0d1038
Florian Sanders <florian.sanders@clever-cloud.com>
2023-07-11 15:33:14 +0200

    feat(cc-ft): uncontrolled version 1 (no sync between input and component state)

---
+++ src/components/cc-ft/cc-ft-uncontrolled.smart.js
@@ -0,0 +1,72 @@
+import { defineSmartComponent } from '../../lib/define-smart-component.js';
+import '../cc-smart-container/cc-smart-container.js';
+import './cc-ft-uncontrolled.js';
+
+defineSmartComponent({
+  selector: 'cc-ft-uncontrolled',
+  params: {
+    fake: { type: String },
+  },
+  onContextUpdate ({ component, context, onEvent, updateComponent, signal }) {
+    const api = getApi();
+
+    /* const validators = {
+      name (value) {
+        if (value == null || value.length === 0) {
+          return 'empty';
+        }
+        return null;
+      },
+      email (value) {
+        return validateEmailAddress(value);
+      },
+    }; */
+
+    onEvent('cc-ft-uncontrolled:submit', () => {
+      updateComponent('formState', (formState) => {
+        formState.state = 'submitting';
+      });
+
+      api.submitForm()
+        .then(() => {
+          component.resetFormState();
+        })
+        .catch((error) => {
+          // todo: error
+          if (error.message === 'email-used') {
+            updateComponent('formState', (formState) => {
+              formState.state = 'idle';
+              formState.email.error = 'already-used';
+            });
+            // component.focusFormItem('email');
+          }
+        });
+    });
+  },
+});
+
+// -- API calls
+function getApi () {
+  return {
+    submitForm () {
+      // return wait(500);
+      return wait(500);
+    },
+  };
+}
+
+function wait (ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
+
+function fail (ms) {
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      reject(new Error('email-used'));
+    }, ms);
+  });
+}
