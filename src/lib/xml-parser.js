03197842097c25a4226184ae99469a9ab1bd76fe
Galimede <mat.degand@outlook.fr>
2023-10-18 17:33:19 +0200

    fix(cc-article-list): fix broken XML with the new website
    Fixes #867

--- src/lib/xml-parser.js
+++ src/lib/xml-parser.js
@@ -9,24 +9,33 @@
  * @returns {Article[]}
  */
 export function parseRssFeed (xmlStr, limit = 9) {
+
+  if (limit <= 0) {
+    return [];
+  }
+
   const xmlStrTrimmed = xmlStr.trim();
   const doc = new DOMParser().parseFromString(xmlStrTrimmed, 'application/xml');
   const error = doc.querySelector('parsererror');
+
   if (error != null) {
     throw new Error(error.innerHTML);
   }
 
   return Array
-    .from(doc.querySelectorAll('item'))
+    .from(doc.documentElement.querySelectorAll('item'))
     .map((node) => {
       const title = node.querySelector('title').textContent;
       const link = node.querySelector('link').textContent;
       const dateRaw = node.querySelector('pubDate').textContent;
       const date = new Date(dateRaw).toISOString();
+
       const descriptionText = node.querySelector('description').childNodes[0].data;
       const descriptionNode = new DOMParser().parseFromString(descriptionText, 'text/html');
-      const banner = descriptionNode.body?.querySelector('.featured-image img')?.src ?? null;
-      const description = descriptionNode.body.querySelector('p').textContent;
+      const banner = descriptionNode.body?.querySelector('.wp-post-image')?.src ?? null;
+      // TODO: we shouldn't have to do the `??` part here but somehow as of 2023-10-18 an article is causing some trouble. We'll have to keep track of this.
+      const description = descriptionNode.body?.querySelectorAll('p')?.[1]?.textContent ?? descriptionNode.body.textContent;
+
       return { title, link, date, banner, description };
     })
     .slice(0, limit);
