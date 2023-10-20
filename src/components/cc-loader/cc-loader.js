d69a36a3e5fbe589cef9e31f871095f9ed3102ba
Pierre DE SOYRES <pierre.de-soyres@clever-cloud.com>
2023-10-20 11:37:42 +0200

    feat(cc-loader): add accessibleName property
    Fixes #870

--- src/components/cc-loader/cc-loader.js
+++ src/components/cc-loader/cc-loader.js
@@ -13,6 +13,53 @@ import { css, html, LitElement } from 'lit';
  */
 export class CcLoader extends LitElement {
 
+  static get properties () {
+    return {
+      accessibleName: { type: String, attribute: 'accessible-name' },
+    };
+  }
+
+  constructor () {
+    super();
+
+    /** @type {string|null} Only use this prop if your loader provides information that is not already given in its surrounding text.
+     *
+     * If this prop has a value:
+     *
+     *  * sets a `role="img"` and `aria-label="the value of this prop"` attributes on the `<svg>` element.
+     *  * creates a `<title>` element inside the `<svg>`.
+     *
+     * This allows assistive technologies to recognize the element as an image and convey its information.
+     *
+     * If this prop has no value, sets an `aria-hidden="true"` attribute on the `<svg>` element so that it can be ignored by assistive technologies.
+     */
+    this.accessibleName = null;
+  }
+
+  updated (changedProperties) {
+    if (changedProperties.has('accessibleName')) {
+      const svg = this.shadowRoot.querySelector('svg');
+
+      if (this.accessibleName == null || this.accessibleName === '') {
+        svg.setAttribute('aria-hidden', 'true');
+        svg.removeAttribute('aria-label');
+        svg.removeAttribute('role');
+        svg.querySelector('title')?.remove();
+      }
+      else {
+        svg.removeAttribute('aria-hidden');
+        svg.setAttribute('aria-label', this.accessibleName);
+        svg.setAttribute('role', 'img');
+        let title = svg.querySelector('title');
+        if (title == null) {
+          title = document.createElement('title');
+          svg.prepend(title);
+        }
+        title.textContent = this.accessibleName;
+      }
+    }
+  }
+
   render () {
     // language=HTML
     return html`
