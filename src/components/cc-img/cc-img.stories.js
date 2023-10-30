c8586f91231758b2325bedae4513cdd25f4ec8cd
Helene Amouzou <helene.amouzou@clever-cloud.com>
2023-10-30 17:06:37 +0100

    refactor(cc-img)!: replace prop text by accessibleName

--- src/components/cc-img/cc-img.stories.js
+++ src/components/cc-img/cc-img.stories.js
@@ -22,24 +22,32 @@ const conf = {
 
 export const defaultStory = makeStory(conf, {
   items: [
-    { text: 'OMG' },
+    { accessibleName: 'OMG' },
+    { accessibleName: 'OMG', skeleton: true },
+    { accessibleName: 'OMG', skeleton: true, src: 'http://placekitten.com/200/200' },
+  ],
+});
+
+export const noAccessibleName = makeStory(conf, {
+  items: [
+    { },
     { skeleton: true },
-    { text: 'OMG', skeleton: true, src: 'http://placekitten.com/200/200' },
+    { skeleton: true, src: 'http://placekitten.com/200/200' },
   ],
 });
 
 export const imageFitContain = makeStory(conf, {
   items: [
-    { text: 'CC', src: 'https://assets.clever-cloud.com/infra/clever-cloud.svg', style: 'border: 1px solid #000; height: 7em; width: 2em;' },
-    { text: 'CC', src: 'https://assets.clever-cloud.com/infra/clever-cloud.svg', style: 'border: 1px solid #000; height: 2em; width: 7em;' },
-    { text: 'CC', src: 'https://assets.clever-cloud.com/infra/clever-cloud.svg', style: 'border: 1px solid #000; height: 7em; width: 2em; --cc-img-fit: contain;' },
-    { text: 'CC', src: 'https://assets.clever-cloud.com/infra/clever-cloud.svg', style: 'border: 1px solid #000; height: 2em; width: 7em; --cc-img-fit: contain;' },
+    { accessibleName: 'CC', src: 'https://assets.clever-cloud.com/infra/clever-cloud.svg', style: 'border: 1px solid #000; height: 7em; width: 2em;' },
+    { accessibleName: 'CC', src: 'https://assets.clever-cloud.com/infra/clever-cloud.svg', style: 'border: 1px solid #000; height: 2em; width: 7em;' },
+    { accessibleName: 'CC', src: 'https://assets.clever-cloud.com/infra/clever-cloud.svg', style: 'border: 1px solid #000; height: 7em; width: 2em; --cc-img-fit: contain;' },
+    { accessibleName: 'CC', src: 'https://assets.clever-cloud.com/infra/clever-cloud.svg', style: 'border: 1px solid #000; height: 2em; width: 7em; --cc-img-fit: contain;' },
   ],
 });
 
 export const noImage = makeStory(conf, {
-  docs: 'If `src` and `skeleton` are not defined, the `text` is displayed. Please make sure it fits the size you defined for the image.',
-  items: [{ text: 'OMG' }],
+  docs: 'If `src` and `skeleton` are not defined, the `accessibleName` is displayed. Please make sure it fits the size you defined for the image.',
+  items: [{ accessibleName: 'OMG' }],
 });
 
 export const loading = makeStory(conf, {
@@ -49,7 +57,7 @@ It's up to you to set \`skeleton\` while you're waiting for the URL of the image
 * The skeleton state will stay after the \`src\` is set, while waiting for the image to load.
 * \`skeleton\` will be set back to \`false\` by the component once the image is loaded is loaded (success or error).
 `,
-  items: [{ text: 'OMG', skeleton: true }],
+  items: [{ accessibleName: 'OMG', skeleton: true }],
 });
 
 export const simulationWithSquareThenOther = makeStory(conf, {
@@ -58,7 +66,7 @@ export const simulationWithSquareThenOther = makeStory(conf, {
 1. load a square image
 1. load another image
 `,
-  items: [{ text: 'OMG', skeleton: true }],
+  items: [{ accessibleName: 'OMG', skeleton: true }],
   simulations: [
     storyWait(3000, ([component]) => {
       component.src = 'http://placekitten.com/200/200';
@@ -70,7 +78,7 @@ export const simulationWithSquareThenOther = makeStory(conf, {
 });
 
 export const simulationWithPortraitThenLandscape = makeStory(conf, {
-  items: [{ text: 'OMG', skeleton: true }],
+  items: [{ accessibleName: 'OMG', skeleton: true }],
   simulations: [
     storyWait(3000, ([component]) => {
       component.src = 'http://placekitten.com/200/500';
@@ -83,7 +91,7 @@ export const simulationWithPortraitThenLandscape = makeStory(conf, {
 });
 
 export const simulationWithLoadingThenError = makeStory(conf, {
-  items: [{ text: 'ERR', skeleton: true }],
+  items: [{ accessibleName: 'ERR', skeleton: true }],
   simulations: [
     storyWait(3000, ([component]) => {
       component.src = 'http://placekitten.com/bad/url';
@@ -93,6 +101,7 @@ export const simulationWithLoadingThenError = makeStory(conf, {
 
 enhanceStoriesNames({
   defaultStory,
+  noAccessibleName,
   imageFitContain,
   noImage,
   loading,
