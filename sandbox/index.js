48a84687e2f265c4837b011a911630119de97439
Pierre DE SOYRES <pierre.de-soyres@clever-cloud.com>
2023-12-07 12:19:21 +0100

    chore(sandbox): add support to smart components in sandboxes

--- sandbox/index.js
+++ sandbox/index.js
@@ -1,10 +1,12 @@
 import { addTranslations, setLanguage } from '../src/lib/i18n.js';
+import { updateRootContext } from '../src/lib/smart-manager.js';
 import { lang, translations } from '../src/translations/translations.en.js';
 import '../src/components/cc-toaster/cc-toaster.js';
 import '../src/components/cc-notice/cc-notice.js';
 
 addTranslations(lang, translations);
 setLanguage(lang);
+updateRootContext({});
 
 window.addEventListener('cc:notify', (event) => {
   document.querySelector('cc-toaster').show(event.detail);
