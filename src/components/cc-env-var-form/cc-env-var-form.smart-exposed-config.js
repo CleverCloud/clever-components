0425b6c741e9b3bf5ae4d3c578763a9ad64e7042
Pierre DE SOYRES <pierre.de-soyres@clever-cloud.com>
2024-01-05 18:06:24 +0100

    refactor(cc-env-var-form)!: migrate to the new smart component design
    BREAKING CHANGE: the properties have changed
    - `state`: new property containing the whole state
    - `variables`: property has been deleted as it is now part of the state
    - `error`: property has been deleted as it is now part of the state
    - `parserOptions`: property has been deleted as it is now part of the state
    - `saving`: property has been deleted as it is now part of the state
    
    Fixes #926

--- src/components/cc-env-var-form/cc-env-var-form.smart-exposed-config.js
+++ src/components/cc-env-var-form/cc-env-var-form.smart-exposed-config.js
@@ -3,75 +3,55 @@ import '../cc-smart-container/cc-smart-container.js';
 import { getAllExposedEnvVars, updateAllExposedEnvVars } from '@clevercloud/client/esm/api/v2/application.js';
 import { toNameValueObject } from '@clevercloud/client/esm/utils/env-vars.js';
 import { fetchApp } from '../../lib/api-helpers.js';
-import { defineSmartComponentWithObservables } from '../../lib/define-smart-component-with-observables.js';
+import { defineSmartComponent } from '../../lib/define-smart-component.js';
 import { i18n } from '../../lib/i18n.js';
 import { notifyError, notifySuccess } from '../../lib/notifications.js';
-import { fromCustomEvent, LastPromise, merge, unsubscribeWithSignal, withLatestFrom } from '../../lib/observables.js';
 import { sendToApi } from '../../lib/send-to-api.js';
 
-defineSmartComponentWithObservables({
+defineSmartComponent({
   selector: 'cc-env-var-form[context="exposed-config"]',
   params: {
     apiConfig: { type: Object },
     ownerId: { type: String },
     appId: { type: String },
   },
-  onConnect (container, component, context$, disconnectSignal) {
+  onContextUpdate ({ context, onEvent, updateComponent, signal }) {
 
-    const app_lp = new LastPromise();
-    const variables_lp = new LastPromise();
+    updateComponent('state', { type: 'loading' });
 
-    const error$ = merge(app_lp.error$, variables_lp.error$);
+    const { apiConfig, ownerId, appId } = context;
 
-    const onSubmit$ = fromCustomEvent(component, 'cc-env-var-form:submit')
-      .pipe(withLatestFrom(context$));
+    Promise.all([
+      fetchApp({ apiConfig, signal, ownerId, appId }),
+      fetchExposedConfig({ apiConfig, signal, ownerId, appId }),
+    ])
+      .then(([app, variables]) => {
+        updateComponent('appName', app.name);
+        updateComponent('state', { type: 'loaded', validationMode: 'simple', variables });
+      })
+      .catch((error) => {
+        console.error(error);
+        updateComponent('state', { type: 'error' });
+      });
 
-    unsubscribeWithSignal(disconnectSignal, [
-
-      error$.subscribe(console.error),
-      error$.subscribe(() => {
-        component.error = true;
-        component.saving = false;
-      }),
-      app_lp.value$.subscribe((app) => (component.appName = app.name)),
-      variables_lp.value$.subscribe((variables) => {
-        component.variables = variables;
-        component.saving = false;
-      }),
-
-      onSubmit$.subscribe(([variables, { apiConfig, ownerId, appId }]) => {
-
-        component.error = false;
-        component.saving = true;
-
-        updateExposedConfig({ apiConfig, ownerId, appId, variables })
-          .then(() => {
-            component.variables = variables;
-            notifySuccess(i18n('cc-env-var-form.update.success'));
-          })
-          .catch(() => notifyError(i18n('cc-env-var-form.update.error')))
-          .finally(() => {
-            component.saving = false;
+    onEvent('cc-env-var-form:submit', (variables) => {
+      updateComponent('state', (state) => {
+        state.type = 'saving';
+      });
+      updateExposedConfig({ apiConfig, ownerId, appId, variables })
+        .then(() => {
+          updateComponent('state', (state) => {
+            state.variables = variables;
           });
-      }),
-
-      context$.subscribe(({ apiConfig, ownerId, appId }) => {
-
-        component.error = false;
-        component.saving = false;
-        component.variables = null;
-
-        if (apiConfig != null && ownerId != null && appId != null) {
-          app_lp.push((signal) => {
-            return fetchApp({ apiConfig, signal, ownerId, appId });
+          notifySuccess(i18n('cc-env-var-form.update.success'));
+        })
+        .catch(() => notifyError(i18n('cc-env-var-form.update.error')))
+        .finally(() => {
+          updateComponent('state', (state) => {
+            state.type = 'loaded';
           });
-          variables_lp.push((signal) => {
-            return fetchExposedConfig({ apiConfig, signal, ownerId, appId });
-          });
-        }
-      }),
-
-    ]);
+        });
+    });
   },
 });
 
--- src/components/cc-env-var-form/cc-env-var-form.types.d.ts
+++ src/components/cc-env-var-form/cc-env-var-form.types.d.ts
@@ -1,1 +1,25 @@
-type ContextType = "env-var" | "env-var-simple" | "env-var-addon" | "exposed-config" | "config-provider";
+import { EnvVarValidationMode, Variable } from "../common.types";
+
+export type EnvVarFormState = EnvVarFormStateLoading | EnvVarFormStateLoaded | EnvVarFormStateSaving | EnvVarFormStateError;
+
+interface EnvVarFormStateLoading {
+  type: 'loading';
+}
+
+interface EnvVarFormStateLoaded {
+  type: 'loaded';
+  variables: Array<Variable>;
+  validationMode: EnvVarValidationMode;
+}
+
+interface EnvVarFormStateSaving {
+  type: 'saving';
+  variables: Array<Variable>;
+  validationMode: EnvVarValidationMode;
+}
+
+interface EnvVarFormStateError {
+  type: 'error';
+}
+
+type EnvVarFormContextType = "env-var" | "env-var-simple" | "env-var-addon" | "exposed-config" | "config-provider";
