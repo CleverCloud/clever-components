010500828442144e09ed4325025ae72244457f49
Florian Sanders <florian.sanders@clever-cloud.com>
2024-01-11 11:56:51 +0100

    feat(form-submit-directive): throw error if not used on a `form` element

--- src/components/cc-ft/form/form-submit-directive.js
+++ src/components/cc-ft/form/form-submit-directive.js
@@ -1,6 +1,10 @@
 import { nothing } from 'lit';
-import { AsyncDirective, directive } from 'lit/async-directive.js';
+import { AsyncDirective, directive, PartType } from 'lit/async-directive.js';
 import { EventHandler } from '../../../lib/events.js';
+
+/**
+ * @typedef {import('lit/async-directive.js').ElementPart} ElementPart
+ */
 
 /**
  * Lit directive helping in handling form
@@ -8,6 +12,10 @@ import { EventHandler } from '../../../lib/events.js';
 class FormSubmitDirective extends AsyncDirective {
   constructor (partInfo) {
     super(partInfo);
+
+    if (partInfo.type !== PartType.ELEMENT || partInfo.element.tagName !== 'FORM') {
+      throw new Error('The `formSubmit` directive must be used on a `form` element');
+    }
 
     this._submitHandler = null;
 
