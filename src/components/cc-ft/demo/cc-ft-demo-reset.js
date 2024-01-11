21d43ab64ef49e9faed9557a180ff4222bf4234b
Florian Sanders <florian.sanders@clever-cloud.com>
2024-01-11 12:24:58 +0100

    feat(cc-ft-demo-reset): use the `formSubmit` directive

--- src/components/cc-ft/demo/cc-ft-demo-reset.js
+++ src/components/cc-ft/demo/cc-ft-demo-reset.js
@@ -1,7 +1,22 @@
 import { css, html, LitElement } from 'lit';
 import '../../cc-button/cc-button.js';
 import '../../cc-input-text/cc-input-text.js';
-import { formSubmitHandler } from '../form/form.js';
+import { formSubmitHandler, formSubmit } from '../form/form.js';
+
+const colorsSelectOptions = [
+  {
+    label: 'red',
+    value: 'red',
+  },
+  {
+    label: 'yellow',
+    value: 'yellow',
+  },
+  {
+    label: 'green',
+    value: 'green',
+  },
+];
 
 export class CcFtDemoReset extends LitElement {
   static get properties () {
@@ -9,16 +24,13 @@ export class CcFtDemoReset extends LitElement {
     };
   }
 
-  constructor () {
-    super();
-  }
-
   render () {
     return html`
-      <form name="my-form" novalidate @submit=${formSubmitHandler}>
-        <cc-input-text label="Name" required name="name"></cc-input-text>
+      <form name="my-form" ${formSubmit(formSubmitHandler(this))}>
+        <cc-input-text label="Name" required name="name" reset-value="prepopulated name"></cc-input-text>
         <cc-input-text label="Surname" required name="surname"></cc-input-text>
         <cc-input-text label="Country" required name="country"></cc-input-text>
+        <cc-select label="Color" required name="color" .options=${colorsSelectOptions}></cc-select>
         
         <div class="buttons-bar">
           <cc-button type="reset">Reset</cc-button>
