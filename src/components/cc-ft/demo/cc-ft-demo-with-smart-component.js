496ebba27f767756e723ed49e26b4f3ecf5b4d68
Florian Sanders <florian.sanders@clever-cloud.com>
2024-01-11 12:30:24 +0100

    feat(cc-ft-demo-with-smart-component): use the `formSubmit` directive

--- src/components/cc-ft/demo/cc-ft-demo-with-smart-component.js
+++ src/components/cc-ft/demo/cc-ft-demo-with-smart-component.js
@@ -3,19 +3,22 @@ import '../../cc-button/cc-button.js';
 import '../../cc-input-text/cc-input-text.js';
 import { defineSmartComponent } from '../../../lib/define-smart-component.js';
 import { updateRootContext } from '../../../lib/smart-manager.js';
-import { formSubmitHandler } from '../form/form.js';
+import { formSubmit, formSubmitHandler } from '../form/form.js';
 
 export class CcFtDemoWithSmartComponent extends LitElement {
   static get properties () {
     return {
-      state: { type: String },
+      form1State: { type: String, attribute: 'form1-state' },
+      form2State: { type: String, attribute: 'form2-state' },
     };
   }
 
   constructor () {
     super();
 
-    this.state = 'idle';
+    this.form1State = 'idle';
+
+    this.form2State = 'idle';
   }
 
   get forms () {
@@ -28,26 +31,50 @@ export class CcFtDemoWithSmartComponent extends LitElement {
   }
 
   render () {
-    const isSubmitting = this.state === 'submitting';
+    const isForm1Submitting = this.form1State === 'submitting';
+    const isForm2Submitting = this.form2State === 'submitting';
+
     return html`
-        <form name="my-form" novalidate @submit=${formSubmitHandler}>
+      <cc-smart-container context="${{ fake: 'toto' }}">
+        <form name="form1" ${formSubmit(formSubmitHandler(this))}>
           <cc-input-text
             label="Name"
             name="name"
-            resetValue=""
+            reset-value=""
             required
-            ?disabled=${isSubmitting}
+            ?disabled=${isForm1Submitting}
           ></cc-input-text>
           <cc-input-text
             type="email"
             label="Email"
             name="email"
-            resetValue=""
+            reset-value=""
             required
-            ?disabled=${isSubmitting}
+            ?disabled=${isForm1Submitting}
           ></cc-input-text>
 
-          <cc-button primary type="submit" ?waiting=${isSubmitting}>Submit</cc-button>
+          <cc-button primary type="submit" ?waiting=${isForm1Submitting}>Submit</cc-button>
+        </form>  
+        <form name="form2" ${formSubmit(formSubmitHandler(this))}>
+          <cc-input-text
+            label="Name"
+            name="name"
+            resetValue=""
+            required
+            ?disabled=${isForm2Submitting}
+          >
+            <p slot="help">Anything but "toto"</p>
+          </cc-input-text>
+          <cc-input-text
+            type="email"
+            label="Email"
+            name="email"
+            reset-value="prepopulated-email@email.fr"
+            required
+            ?disabled=${isForm2Submitting}
+          ></cc-input-text>
+
+          <cc-button primary type="submit" ?waiting=${isForm2Submitting}>Submit</cc-button>
         </form>  
       </cc-smart-container>
     `;
@@ -64,6 +91,7 @@ export class CcFtDemoWithSmartComponent extends LitElement {
         form {
           display: flex;
           flex-direction: column;
+          margin-bottom: 2em;
           gap: 0.5em;
         }
       `,
@@ -92,29 +120,49 @@ defineSmartComponent({
    */
   onContextUpdate ({ component, context, onEvent, updateComponent, signal }) {
 
-    onEvent('cc-ft-demo-with-smart-component:formSubmit', ({ data }) => {
-      component.state = 'submitting';
-      const formElement = component.shadowRoot.querySelector('form[name=my-form]');
+    onEvent('cc-ft-demo-with-smart-component:formSubmit', ({ form, data }) => {
+      console.log('submitting', data);
+      const formElement = component.shadowRoot.querySelector(`form[name=${form}]`);
 
-      submitForm(data)
-        .then(() => {
-          component.state = 'idle';
-          formElement.reset();
-        })
-        .catch((error) => {
-          if (error.message === 'email-used') {
-            component.state = 'idle';
-            // should our component expose some kind of helper for this?
-            formElement.elements.email.errorMessage = 'Email already used';
-            formElement.elements.email.focus();
-          }
-        });
+      if (form === 'form1') {
+        component.form1State = 'submitting';
+        submitForm1(data)
+          .then(() => {
+            component.form1State = 'idle';
+            formElement.reset();
+          })
+          .catch((error) => {
+            if (error.message === 'email-used') {
+              component.form1State = 'idle';
+              // should our component expose some kind of helper for this?
+              formElement.elements.email.errorMessage = 'Email already used';
+              formElement.elements.email.focus();
+            }
+          });
+      }
+
+      if (form === 'form2') {
+        component.form2State = 'submitting';
+        submitForm2(data)
+          .then(() => {
+            component.form2State = 'idle';
+            formElement.reset();
+          })
+          .catch((error) => {
+            if (error.message === 'awful-name') {
+              component.form2State = 'idle';
+              // should our component expose some kind of helper for this?
+              formElement.elements.name.errorMessage = 'I do not like your name, please change it!';
+              formElement.elements.name.focus();
+            }
+          });
+      }
     });
   },
 });
 
 // -- API calls
-function submitForm ({ name, email }) {
+function submitForm1 ({ name, email }) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (email.startsWith('used')) {
@@ -124,5 +172,18 @@ function submitForm ({ name, email }) {
         resolve();
       }
     }, 500);
+  });
+}
+
+function submitForm2 ({ name, email }) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (name === 'toto') {
+        reject(new Error('awful-name'));
+      }
+      else {
+        resolve();
+      }
+    }, 600);
   });
 }
