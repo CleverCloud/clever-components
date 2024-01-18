31b108cdc35ddeb546cc13ba4d703d7e968e6b71
Florian Sanders <florian.sanders@clever-cloud.com>
2024-01-18 18:36:51 +0100

    fix(cc-ft-demo-with-native-inputs): clear error messages on submit

--- src/components/cc-ft/demo/cc-ft-demo-with-native-inputs.js
+++ src/components/cc-ft/demo/cc-ft-demo-with-native-inputs.js
@@ -2,21 +2,20 @@ import { css, html, LitElement } from 'lit';
 import { formSubmit, formSubmitHandler } from '../form/form.js';
 
 /**
- * @param {String[]} selectedCheckboxes - names of the selected checkboxes
- * @returns {String|undefined} returns the error message to display or undefined if no error
+ * @param {string[]} selectedCheckboxes - names of the selected checkboxes
+ * @returns {string|undefined} returns the error message to display or undefined if no error
  */
 const validateCheckboxGroup = function (selectedCheckboxes) {
   if (selectedCheckboxes == null || selectedCheckboxes.length === 0) {
     return 'Please select at least one option';
   }
-  // TODO: We have to return `null` or undefined (but this makes eslint unhappy) because `formSubmitHandler` expects valid result to be `null` / `undefined`
-  return null;
 };
 
 export class CcFtDemoWithNativeInputs extends LitElement {
   static get properties () {
     return {
       _checkboxError: { type: String, state: true },
+      _radioError: { type: String, state: true },
     };
   }
 
@@ -25,18 +24,33 @@ export class CcFtDemoWithNativeInputs extends LitElement {
 
     /** @type {string|null} the error message for the checkbox group */
     this._checkboxError = null;
+
+    /** @type {string|null} the error message for the radio group */
+    this._radioError = null;
   }
 
   _onInvalid ({ detail }) {
-    const invalidTest = detail.find((d) => d.name === 'checkbox' && d.validationResult.valid === false);
-    this._checkboxError = invalidTest != null
-      ? invalidTest.validationResult.code
+    const invalidCheckboxes = detail.find((d) => d.name === 'checkbox' && d.validationResult.valid === false);
+    const invalidRadios = detail.find((d) => d.name === 'radio' && d.validationResult.valid === false);
+    this._checkboxError = invalidCheckboxes != null
+      ? invalidCheckboxes.validationResult.code
       : null;
+
+    this._radioError = invalidRadios != null
+      ? 'Please select one of the available options'
+      : null;
+  }
+
+  // TODO: we only provide an invalid event and no valid event so right now the only way to remove error messages if everything went well is to use the submit event
+  _onSubmit (e) {
+    e.preventDefault();
+    this._checkboxError = null;
+    this._radioError = null;
   }
 
   render () {
     return html`
-      <form ${formSubmit(formSubmitHandler(this, { checkbox: validateCheckboxGroup }))} @form:invalid=${this._onInvalid}>
+      <form ${formSubmit(formSubmitHandler(this, { checkbox: validateCheckboxGroup }))} @form:invalid=${this._onInvalid} @submit=${this._onSubmit}>
         <label for="name">Name:</label><input id="name" type="text" required name="name" />
         
         <label for="color">Favorite Color:</label> <select required name="color" >
@@ -54,7 +68,7 @@ export class CcFtDemoWithNativeInputs extends LitElement {
         <fieldset class="group" tabindex="-1">
           <legend>test (Required)</legend>
           ${this._checkboxError != null && this._checkboxError.length > 0
-            ? this._checkboxError
+            ? html`<p id="checkboxes-error">${this._checkboxError}</p>`
             : ''
           }
           <!-- 
@@ -64,28 +78,31 @@ export class CcFtDemoWithNativeInputs extends LitElement {
             Thankfully, our directive provides a way to pass a custom function to validate a group of fields so this is easily solved.
           -->
           <div>
-            <input type="checkbox" name="checkbox" value="option1" id="checkbox-option1"/><label for="checkbox-option1">Option1</label>
+            <input type="checkbox" name="checkbox" value="option1" id="checkbox-option1" aria-describedby="checkboxes-error" /><label for="checkbox-option1">Option1</label>
           </div>
           <div>
-            <input type="checkbox" name="checkbox" value="option2" id="checkbox-option2"/><label for="checkbox-option2">Option2</label>
+            <input type="checkbox" name="checkbox" value="option2" id="checkbox-option2" aria-describedby="checkboxes-error" /><label for="checkbox-option2">Option2</label>
           </div>
           <div>
-            <input type="checkbox" name="checkbox" value="option3" id="checkbox-option3"/><label for="checkbox-option3">Option3</label>
+            <input type="checkbox" name="checkbox" value="option3" id="checkbox-option3" aria-describedby="checkboxes-error" /><label for="checkbox-option3">Option3</label>
           </div>
         </fieldset>
 
         <fieldset class="group" tabindex="-1">
           <legend>test 2 (Required)</legend>
+          ${this._radioError != null && this._radioError.length > 0
+            ? html`<p id="radio-error">${this._radioError}</p>`
+            : ''
+          }
           <div>
-            <input type="radio" name="radio" value="option1" id="radio-option1" required /><label for="radio-option1">Option1</label>
+            <input type="radio" name="radio" value="option1" id="radio-option1" required aria-describedby="radio-error" /><label for="radio-option1">Option1</label>
           </div>
           <div>
-            <input type="radio" name="radio" value="option2" id="radio-option2" required /><label for="radio-option2">Option2</label>
+            <input type="radio" name="radio" value="option2" id="radio-option2" required aria-describedby="radio-error" /><label for="radio-option2">Option2</label>
           </div>
           <div>
-            <input type="radio" name="radio" value="option3" id="radio-option3" required /><label for="radio-option3">Option3</label>
+            <input type="radio" name="radio" value="option3" id="radio-option3" required aria-describedby="radio-error" /><label for="radio-option3">Option3</label>
           </div>
-          <!-- TODO: error message inline -->
         </fieldset>
 
         <button type="submit">Submit</button>
@@ -107,8 +124,18 @@ export class CcFtDemoWithNativeInputs extends LitElement {
           gap: 0.5em;
         }
 
-        fieldset:focus {
+        *:focus {
+          outline: solid 2px black;
+        }
+
+        /* TODO: this shows why the :invalid pseudo-class is not great for styling error states */
+        *:invalid:focus {
           outline: solid 2px red;
+        }
+
+        p {
+          margin: 0.5em;
+          color: red;
         }
       `,
     ];
