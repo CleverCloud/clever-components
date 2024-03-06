df908481720764bc31635e71ef41da7b983291fb
Helene Amouzou <helene.amouzou@clever-cloud.com>
2024-03-06 12:13:51 +0100

    refactor(cc-article-card)!: rework properties to avoid impossible states
    BREAKING CHANGE: the properties have changed
    - `state`: new property containing the whole state
    - `title`: property has been deleted as it is now part of the state
    - `description`: property has been deleted as it is now part of the state
    - `banner`: property has been deleted as it is now part of the state
    - `date`: property has been deleted as it is now part of the state
    - `link`: property has been deleted as it is now part of the state

--- src/components/cc-article-card/cc-article-card.stories.js
+++ src/components/cc-article-card/cc-article-card.stories.js
@@ -1,21 +1,6 @@
 import './cc-article-card.js';
 import { makeStory, storyWait } from '../../stories/lib/make-story.js';
 
-const DEFAULT_ARTICLE = {
-  title: 'Announcing the New Middle East Zone in Jeddah',
-  description: 'Today we are officially announcing the opening of a new Clever Cloud zone located in Jeddah, our first in the Middle East region.',
-  banner: 'https://cdn.clever-cloud.com/uploads/2022/01/jeddah-zone-en.png',
-  date: 'Tue, 22 Mar 2022 08:39:00 +0000',
-  link: 'https://www.clever-cloud.com/blog/features/2022/01/27/announcing-the-new-middle-east-zone/',
-};
-const OTHER_ARTICLE = {
-  title: 'Rethinking invoices to empower your accountant',
-  description: 'Earlier this week, we released some changes in how we present our invoices. The result: invoices that help you understand what you pay and what to expect next time.',
-  banner: 'https://cdn.clever-cloud.com/uploads/2022/02/Refactoring-invoices-2.png',
-  date: 'Wed, 16 Feb 2022 08:51:44 +0000',
-  link: 'https://www.clever-cloud.com/blog/features/2022/02/16/rethinking-invoices-to-empower-your-accountant/',
-};
-
 export default {
   tags: ['autodocs'],
   title: '🛠 homepage/<cc-article-card>',
@@ -33,34 +18,60 @@ const conf = {
   `,
 };
 
+/**
+ * @typedef {import('./cc-article-card.js').CcArticleCard} CcArticleCard
+ * @typedef {import('./cc-article-card.types.js').ArticleCardStateLoaded} ArticleCardStateLoaded
+ * @typedef {import('./cc-article-card.types.js').ArticleCardStateLoading} ArticleCardStateLoading
+ */
+
+/** @type {{ state: ArticleCardStateLoaded }} */
+const DEFAULT_ARTICLE = {
+  state: {
+    type: 'loaded',
+    title: 'Announcing the New Middle East Zone in Jeddah',
+    description: 'Today we are officially announcing the opening of a new Clever Cloud zone located in Jeddah, our first in the Middle East region.',
+    banner: 'https://cdn.clever-cloud.com/uploads/2022/01/jeddah-zone-en.png',
+    date: 'Tue, 22 Mar 2022 08:39:00 +0000',
+    link: 'https://www.clever-cloud.com/blog/features/2022/01/27/announcing-the-new-middle-east-zone/',
+  },
+};
+
+/** @type {{ state: ArticleCardStateLoaded }} */
+const OTHER_ARTICLE = {
+  state: {
+    type: 'loaded',
+    title: 'Rethinking invoices to empower your accountant',
+    description: 'Earlier this week, we released some changes in how we present our invoices. The result: invoices that help you understand what you pay and what to expect next time.',
+    banner: 'https://cdn.clever-cloud.com/uploads/2022/02/Refactoring-invoices-2.png',
+    date: 'Wed, 16 Feb 2022 08:51:44 +0000',
+    link: 'https://www.clever-cloud.com/blog/features/2022/02/16/rethinking-invoices-to-empower-your-accountant/',
+  },
+};
+
 export const defaultStory = makeStory(conf, {
-  items: [
-    DEFAULT_ARTICLE,
-    OTHER_ARTICLE,
-  ],
+  items: [DEFAULT_ARTICLE, OTHER_ARTICLE],
 });
 
 export const skeleton = makeStory(conf, {
-  items: [{}],
+  items: [{
+    /** @type {ArticleCardStateLoading} */
+    state: { type: 'loading' },
+  }],
 });
 
 // No need to invest time on empty story right now.
 
 export const dataLoaded = makeStory(conf, {
-  items: [
-    DEFAULT_ARTICLE,
-  ],
+  items: [DEFAULT_ARTICLE],
 });
 
 export const simulations = makeStory(conf, {
   items: [{}],
   simulations: [
-    storyWait(2000, ([component]) => {
-      component.title = DEFAULT_ARTICLE.title;
-      component.description = DEFAULT_ARTICLE.description;
-      component.banner = DEFAULT_ARTICLE.banner;
-      component.date = DEFAULT_ARTICLE.date;
-      component.link = DEFAULT_ARTICLE.link;
-    }),
+    storyWait(2000,
+      /** @param {[CcArticleCard]} components */
+      ([component]) => {
+        component.state = DEFAULT_ARTICLE.state;
+      }),
   ],
 });
---
+++ src/components/cc-article-card/cc-article-card.types.d.ts
@@ -0,0 +1,22 @@
+export type ArticleCardState = ArticleCardStateLoaded | ArticleCardStateLoading;
+
+interface ArticleCardStateLoaded {
+    type: 'loaded';
+    banner: string;
+    date: string;
+    description: string;
+    link: string;
+    title: string;
+}
+
+export interface ArticleCardStateLoading {
+    type: 'loading';
+}
+
+export interface SkeletonArticle {
+    banner: null;
+    date: string;
+    description: string;
+    link: null;
+    title: string;
+}
