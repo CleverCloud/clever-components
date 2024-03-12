bb5a45af05b84ad754161d3bd38e5de854833e07
Florian Sanders <florian.sanders@clever-cloud.com>
2024-03-12 15:50:49 +0100

    --wip-- [skip ci]

--- src/components/cc-domain-management/cc-domain-management.js
+++ src/components/cc-domain-management/cc-domain-management.js
@@ -2,8 +2,9 @@ import { css, html, LitElement } from 'lit';
 import '../cc-block/cc-block.js';
 import '../cc-input-text/cc-input-text.js';
 import '../cc-button/cc-button.js';
+import { publicSuffixList } from './public-suffix-list.js';
+import { tldList } from './tld-list.js';
 
-const MATCH = /\../;
 /**
  * @typedef {import('../cc-input-text/cc-input-text.js').CcInputText} CcInputText
  * @typedef {import('./cc-domain-management.types.js').DataListOption} DataListOption
@@ -27,17 +28,49 @@ export class CcDomainManagement extends LitElement {
   }
 
   /**
-   * @param {CustomEvent & { detail: string }} event
+   * @param {Event & { target: CcInputText }} event
    */
-  _onDomainInput ({ detail: value }) {
-    value.toto = 'toto';
+  _onDomainBlur ({ target }) {
+    console.log('blurrr');
+    const value = target.value;
+    // trouver Path
+    // trouver publicSuffix
+    // trouver Tld
+    // trouver domaine
+    // trouver subDomain
+    const domainAsUrl = new URL('https://' + value);
+    const path = domainAsUrl.pathname;
+    const hostname = domainAsUrl.hostname;
+    const publicSuffixes = publicSuffixList.filter((publicSuffix) => {
+      return hostname.endsWith(publicSuffix) && publicSuffix.length > 0;
+    });
+    const tld = tldList.find((tld) => {
+      return hostname.endsWith(tld) && tld.length > 0;
+    });
+    const publicSuffix = publicSuffixes?.sort((a, b) => b.length - a.length)[0];
+
+    console.log({ path, hostname, eTld: publicSuffix ?? tld });
+    // const lastPart = splitDomain.slice(-1)[0];
+    // const tld = tldList.find((tld) => tld === lastPart.toUpperCase());
+    //
+    // if (splitDomain.length > 1 && (publicSuffix !== undefined || tld !== undefined)) {
+    //   const domain = {
+    //     eTld:
+    //     domain: splitDomain.slice(-2)[0],
+    //   };
+    // }
+
+    // extract public list
+    // on prend le reste et on découpe par "."
+    // juste après le etld, on aura le domaine
+    // et après le ou les sous-domaines
   }
 
   render () {
     return html`
       <cc-block>
         <div slot="title">Add a domain</div>
-        <cc-input-text id="toto" label="Domain name" @cc-input-text:input=${this._onDomainInput}></cc-input-text>
+        <cc-input-text id="toto" label="Domain name" @blur=${this._onDomainBlur}></cc-input-text>
         <cc-button>Add domain</cc-button>
         <div class="live-preview">
 
