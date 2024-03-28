9b5add3f907893a5f21610a397681f4a94885841
Helene Amouzou <helene.amouzou@clever-cloud.com>
2024-03-28 11:51:39 +0100

    refactor(cc-heptapod-info)!: rework properties to avoid impossible states
    BREAKING CHANGE: the properties have changed
    - `state`: new property containing the whole state
    - `statistics`: property has been deleted as it is now part of the state
    - `error`: property has been deleted as it is now part of the state

--- src/components/cc-heptapod-info/cc-heptapod-info.types.d.ts
+++ src/components/cc-heptapod-info/cc-heptapod-info.types.d.ts
@@ -1,6 +1,26 @@
-interface Statistics {
+export type HeptapodInfoState = HeptapodInfoStateLoaded | HeptapodInfoStateLoading | HeptapodInfoStateError | HeptapodInfoStateNotUsed;
+
+export interface HeptapodInfoStateLoaded {
+  type : 'loaded';
+  statistics: Statistics;
+}
+
+export interface HeptapodInfoStateLoading {
+  type : 'loading';
+}
+
+export interface HeptapodInfoStateError {
+  type : 'error';
+}
+
+export interface HeptapodInfoStateNotUsed {
+  type : 'not-used';
+}
+
+export interface Statistics {
   privateActiveUsers: number;
   publicActiveUsers: number;
   storage: number;
   price: number;
 }
+
