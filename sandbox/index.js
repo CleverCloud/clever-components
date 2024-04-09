8bd689e03a24337f305c06b7c200f4ccaf32dd88
Pierre DE SOYRES <pierre.de-soyres@clever-cloud.com>
2024-04-09 14:17:28 +0200

    chore(sandbox): add language switcher

--- sandbox/index.js
+++ sandbox/index.js
@@ -1,11 +1,44 @@
 import { addTranslations, setLanguage } from '../src/lib/i18n.js';
 import { updateRootContext } from '../src/lib/smart-manager.js';
-import { lang, translations } from '../src/translations/translations.en.js';
+import { lang as langEn, translations as translationsEn } from '../src/translations/translations.en.js';
+import { lang as langFr, translations as translationsFr } from '../src/translations/translations.fr.js';
 import '../src/components/cc-toaster/cc-toaster.js';
 import '../src/components/cc-notice/cc-notice.js';
+import '../src/components/cc-toggle/cc-toggle.js';
 
-addTranslations(lang, translations);
-setLanguage(lang);
+addTranslations(langEn, translationsEn);
+addTranslations(langFr, translationsFr);
+
+const DEFAULT_LANG = langEn;
+let currentLang = localStorage.getItem('cc-component-lang');
+if (currentLang == null) {
+  currentLang = DEFAULT_LANG;
+  localStorage.setItem('cc-component-lang', currentLang);
+}
+setLanguage(currentLang);
+
+const $langContainer = document.querySelector('.languages');
+
+const languageToggle = document.createElement('cc-toggle');
+languageToggle.choices = [
+  {
+    label: `${translationsEn.LANGUAGE}`,
+    value: langEn,
+  },
+  {
+    label: `${translationsFr.LANGUAGE}`,
+    value: langFr,
+  },
+];
+languageToggle.legend = 'Language';
+languageToggle.value = currentLang;
+languageToggle.addEventListener('cc-toggle:input', ({ detail }) => {
+  localStorage.setItem('cc-component-lang', detail);
+  setLanguage(detail);
+  window.location.reload();
+});
+$langContainer.appendChild(languageToggle);
+
 updateRootContext({});
 
 window.addEventListener('cc:notify', (event) => {
