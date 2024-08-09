fdbd5b5f6ba09474364d590fb460be552ed2c312
Florian Sanders <florian.sanders@clever-cloud.com>
2024-08-09 13:58:18 +0200

    refactor(cc-invoice): typecheck

--- src/components/cc-invoice-list/cc-invoice-list.js
+++ src/components/cc-invoice-list/cc-invoice-list.js
@@ -65,7 +65,7 @@ export class CcInvoiceList extends LitElement {
   }
 
   /**
-   * @param {Event & { detail: string }} event
+   * @param {CustomEvent<string>} event
    * @private
    */
   _onYearFilterValue({ detail: year }) {
