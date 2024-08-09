bf6a9b2690350b611cdbc149aaabf154d1a980e6
Florian Sanders <florian.sanders@clever-cloud.com>
2024-08-09 17:06:55 +0200

    refactor(cc-addon-mongodb-options): typecheck

--- src/components/cc-addon-mongodb-options/cc-addon-mongodb-options.js
+++ src/components/cc-addon-mongodb-options/cc-addon-mongodb-options.js
@@ -6,6 +6,7 @@ import '../cc-addon-option-form/cc-addon-option-form.js';
 
 /**
  * @typedef {import('../common.types.js').AddonOption} AddonOption
+ * @typedef {import('../cc-addon-option-form/cc-addon-option-form.types.js').OptionStates} OptionStates
  */
 /**
  * A component that displays the available options of a MongoDB add-on.
@@ -28,6 +29,7 @@ export class CcAddonMongodbOptions extends LitElement {
     this.options = [];
   }
 
+  /** @param {Event & { detail: OptionStates }} event */
   _onFormOptionsSubmit({ detail }) {
     dispatchCustomEvent(this, 'submit', detail);
   }
