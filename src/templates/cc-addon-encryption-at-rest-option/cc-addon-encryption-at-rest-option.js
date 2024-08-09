1c2ec3fff706fa3780802a41f17c4f5e5cb865bd
Florian Sanders <florian.sanders@clever-cloud.com>
2024-08-09 16:39:37 +0200

    refactor(cc-addon-encryption-at-rest-option): typecheck

--- src/templates/cc-addon-encryption-at-rest-option/cc-addon-encryption-at-rest-option.js
+++ src/templates/cc-addon-encryption-at-rest-option/cc-addon-encryption-at-rest-option.js
@@ -3,6 +3,10 @@ import { iconRemixShieldKeyholeFill as iconEncryptionAtRest } from '../../assets
 import '../../components/cc-icon/cc-icon.js';
 import { i18n } from '../../translations/translation.js';
 
+/**
+ * @typedef {import('../../components/cc-addon-option-form/cc-addon-option-form.js').AddonOption} AddonOption
+
+/** @type {(option: AddonOption) => AddonOption} */
 export const ccAddonEncryptionAtRestOption = ({ enabled }) => {
   const description = html`
     <div class="option-details">${i18n('cc-addon-encryption-at-rest-option.description')}</div>
