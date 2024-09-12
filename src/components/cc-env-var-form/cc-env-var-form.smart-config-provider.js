5c1a880c4c640c48dac5616c2e0c32df897be12d
Florian Sanders <florian.sanders@clever-cloud.com>
2024-09-12 15:58:20 +0200

    refactor(cc-env-var-form-*.smart): typecheck

--- src/components/cc-env-var-form/cc-env-var-form.smart-config-provider.js
+++ src/components/cc-env-var-form/cc-env-var-form.smart-config-provider.js
@@ -1,3 +1,4 @@
+// @ts-expect-error FIXME: remove when clever-client exports types
 import { get as getAddon } from '@clevercloud/client/esm/api/v2/addon.js';
 import { defineSmartComponent } from '../../lib/define-smart-component.js';
 import { notifyError, notifySuccess } from '../../lib/notifications.js';
@@ -6,6 +7,16 @@ import { i18n } from '../../translations/translation.js';
 import '../cc-smart-container/cc-smart-container.js';
 import './cc-env-var-form.js';
 
+/**
+ * @typedef {import('./cc-env-var-form.js').CcEnvVarForm} CcEnvVarForm
+ * @typedef {import('./cc-env-var-form.types.js').EnvVarFormState} EnvVarFormState
+ * @typedef {import('./cc-env-var-form.types.js').EnvVarFormStateLoaded} EnvVarFormStateLoaded
+ * @typedef {import('./cc-env-var-form.types.js').EnvVarFormStateSaving} EnvVarFormStateSaving
+ * @typedef {import('../common.types.js').EnvVar} EnvVar
+ * @typedef {import('../common.types.js').Addon} Addon
+ * @typedef {import('../../lib/send-to-api.js').ApiConfig} ApiConfig
+ */
+
 defineSmartComponent({
   selector: 'cc-env-var-form[context="config-provider"]',
   params: {
@@ -13,76 +24,142 @@ defineSmartComponent({
     ownerId: { type: String },
     addonId: { type: String },
   },
+  /**
+   * @param {Object} settings
+   * @param {CcEnvVarForm} settings.component
+   * @param {{ apiConfig: ApiConfig, ownerId: string, addonId: string }} settings.context
+   * @param {(type: string, listener: (detail: any) => void) => void} settings.onEvent
+   * @param {function} settings.updateComponent
+   * @param {AbortSignal} settings.signal
+   */
+  // @ts-expect-error FIXME: remove once `onContextUpdate` is typed with generics
   onContextUpdate({ context, onEvent, updateComponent, signal }) {
     updateComponent('state', { type: 'loading' });
 
     const { apiConfig, ownerId, addonId } = context;
 
+    /** @type {string} realAddonId */
     let realAddonId = null;
 
     fetchAddon({ apiConfig, ownerId, addonId, signal })
-      .then((addon) => {
-        updateComponent('addonName', addon.name);
-        realAddonId = addon.realId;
-        return fetchVariables({ apiConfig, realAddonId, signal });
-      })
-      .then((variables) => {
-        updateComponent('state', { type: 'loaded', validationMode: 'simple', variables });
-      })
-      .catch((error) => {
-        console.error(error);
-        updateComponent('state', { type: 'error' });
-      });
+      .then(
+        /** @param {Addon} addon */
+        (addon) => {
+          updateComponent('addonName', addon.name);
+          realAddonId = addon.realId;
+          return fetchVariables({ apiConfig, realAddonId, signal });
+        },
+      )
+      .then(
+        /** @param {Array<EnvVar>} variables */
+        (variables) => {
+          updateComponent('state', { type: 'loaded', validationMode: 'simple', variables });
+        },
+      )
+      .catch(
+        /** @param {Error} error */
+        (error) => {
+          console.error(error);
+          updateComponent('state', { type: 'error' });
+        },
+      );
 
     onEvent('cc-env-var-form:submit', (variables) => {
-      updateComponent('state', (state) => {
-        state.type = 'saving';
-      });
+      updateComponent(
+        'state',
+        /** @param {EnvVarFormState} state */
+        (state) => {
+          state.type = 'saving';
+        },
+      );
       updateVariables({ apiConfig, realAddonId, variables })
         .then(() => {
-          updateComponent('state', (state) => {
-            state.variables = variables;
-          });
+          updateComponent(
+            'state',
+            /** @param {EnvVarFormStateSaving} state */
+            (state) => {
+              state.variables = variables;
+            },
+          );
           notifySuccess(i18n('cc-env-var-form.update.success'));
         })
         .catch(() => notifyError(i18n('cc-env-var-form.update.error')))
         .finally(() => {
-          updateComponent('state', (state) => {
-            state.type = 'loaded';
-          });
+          updateComponent(
+            'state',
+            /** @param {EnvVarFormStateLoaded} state */
+            (state) => {
+              state.type = 'loaded';
+            },
+          );
         });
     });
   },
 });
 
+/**
+ * @param {object} params
+ * @param {ApiConfig} params.apiConfig
+ * @param {AbortSignal} params.signal
+ * @param {string} params.ownerId
+ * @param {string} params.addonId
+ * @returns {Promise<Addon>}
+ */
 function fetchAddon({ apiConfig, signal, ownerId, addonId }) {
   return getAddon({ id: ownerId, addonId }).then(sendToApi({ apiConfig, signal }));
 }
 
+/**
+ * @param {object} params
+ * @param {ApiConfig} params.apiConfig
+ * @param {AbortSignal} params.signal
+ * @param {string} params.realAddonId
+ * @returns {Promise<Array<EnvVar>>}
+ */
 async function fetchVariables({ apiConfig, signal, realAddonId }) {
   return getConfigProviderEnv({ realAddonId }).then(sendToApi({ apiConfig, signal }));
 }
 
-async function updateVariables({ apiConfig, signal, realAddonId, variables }) {
-  return updateConfigProviderEnv({ realAddonId }, variables).then(sendToApi({ apiConfig, signal }));
+/**
+ * @param {object} params
+ * @param {ApiConfig} params.apiConfig
+ * @param {string} params.realAddonId
+ * @param {Array<EnvVar>} params.variables
+ * @returns {Promise<Addon>}
+ */
+async function updateVariables({ apiConfig, realAddonId, variables }) {
+  return updateConfigProviderEnv({ realAddonId }, variables).then(sendToApi({ apiConfig }));
 }
 
-// TODO clever-client
-export function getConfigProviderEnv(params) {
+/**
+ * TODO: clever-client
+ *
+ * @param {object} params
+ * @param {string} params.realAddonId
+ * @returns {Promise<{ method: 'get', url: string, headers: { Accept: 'application/json' }}>}
+ */
+export function getConfigProviderEnv({ realAddonId }) {
   return Promise.resolve({
     method: 'get',
-    url: `/v4/addon-providers/config-provider/addons/${params.realAddonId}/env`,
+    url: `/v4/addon-providers/config-provider/addons/${realAddonId}/env`,
     headers: { Accept: 'application/json' },
     // no query params
     // no body
   });
 }
 
-// TODO clever-client
-export function updateConfigProviderEnv(params, body) {
+/**
+ * TODO: clever-client
+ *
+ * @param {object} params
+ * @param {string} params.realAddonId
+ * @param {Array<EnvVar>} body
+ * @returns {Promise<{ method: 'put', url: string, headers: { Accept: 'application/json' }, body: Array<EnvVar> }>}
+ */
+export function updateConfigProviderEnv({ realAddonId }, body) {
   return Promise.resolve({
     method: 'put',
-    url: `/v4/addon-providers/config-provider/addons/${params.realAddonId}/env`,
+    url: `/v4/addon-providers/config-provider/addons/${realAddonId}/env`,
     headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
     // no query params
     body,
--- src/components/cc-env-var-form/cc-env-var-form.smart-env-var-addon.js
+++ src/components/cc-env-var-form/cc-env-var-form.smart-env-var-addon.js
@@ -1,8 +1,15 @@
+// @ts-expect-error FIXME: remove when clever-client exports types
 import { getAllEnvVars } from '@clevercloud/client/esm/api/v2/addon.js';
 import { defineSmartComponent } from '../../lib/define-smart-component.js';
 import { sendToApi } from '../../lib/send-to-api.js';
 import '../cc-smart-container/cc-smart-container.js';
 import './cc-env-var-form.js';
+
+/**
+ * @typedef {import('./cc-env-var-form.js').CcEnvVarForm} CcEnvVarForm
+ * @typedef {import('../common.types.js').EnvVar} EnvVar
+ * @typedef {import('../../lib/send-to-api.js').ApiConfig} ApiConfig
+ */
 
 defineSmartComponent({
   selector: 'cc-env-var-form[context="env-var-addon"]',
@@ -11,22 +18,45 @@ defineSmartComponent({
     ownerId: { type: String },
     addonId: { type: String },
   },
+  /**
+   * @param {Object} settings
+   * @param {CcEnvVarForm} settings.component
+   * @param {{ apiConfig: ApiConfig, ownerId: string, addonId: string }} settings.context
+   * @param {(type: string, listener: (detail: any) => void) => void} settings.onEvent
+   * @param {function} settings.updateComponent
+   * @param {AbortSignal} settings.signal
+   */
+  // @ts-expect-error FIXME: remove once `onContextUpdate` is typed with generics
   onContextUpdate({ context, updateComponent, signal }) {
     updateComponent('state', { type: 'loading' });
 
     const { apiConfig, ownerId, addonId } = context;
 
     fetchEnvVars({ apiConfig, signal, ownerId, addonId })
-      .then((variables) => {
-        updateComponent('state', { type: 'loaded', validationMode: 'simple', variables });
-      })
-      .catch((error) => {
-        console.error(error);
-        updateComponent('state', { type: 'error' });
-      });
+      .then(
+        /** @param {Array<EnvVar>} variables */
+        (variables) => {
+          updateComponent('state', { type: 'loaded', validationMode: 'simple', variables });
+        },
+      )
+      .catch(
+        /** @param {Error} error */
+        (error) => {
+          console.error(error);
+          updateComponent('state', { type: 'error' });
+        },
+      );
   },
 });
 
+/**
+ * @param {object} params
+ * @param {ApiConfig} params.apiConfig
+ * @param {AbortSignal} params.signal
+ * @param {string} params.ownerId
+ * @param {string} params.addonId
+ * @returns {Promise<Array<EnvVar>>}
+ */
 function fetchEnvVars({ apiConfig, signal, ownerId, addonId }) {
   return getAllEnvVars({ id: ownerId, addonId }).then(sendToApi({ apiConfig, signal }));
 }
