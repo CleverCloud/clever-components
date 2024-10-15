1693ce8e758ede9a9cc1863e600b2a06daccbab7
Helene Amouzou <helene.amouzou@clever-cloud.com>
2024-10-15 16:06:12 +0200

    refactor(cc-block-section): adapt component with the new `cc-block`

--- src/components/cc-block-section/cc-block-section.stories.js
+++ src/components/cc-block-section/cc-block-section.stories.js
@@ -29,14 +29,14 @@ export const defaultStory = makeStory(conf, {
   items: [
     {
       innerHTML: `
-      <div slot="title">This is my block</div>
-      <cc-block-section>
+      <div slot="header-title">This is my block</div>
+      <cc-block-section slot="content">
         <div slot="title">Subtitle Foo</div>
         <div slot="info">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque feugiat dui at leo porta dignissim. Etiam ut purus ultrices, pulvinar tellus quis, cursus massa. Mauris dignissim accumsan ex, at vestibulum lectus fermentum id.</div>
         <cc-input-text label="Name:"></cc-input-text>
         <cc-input-text label="Lastname:"></cc-input-text>
       </cc-block-section>
-      <cc-block-section>
+      <cc-block-section slot="content">
         <div slot="title">Subtitle Bar</div>
         <div slot="info">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque feugiat dui at leo porta dignissim. Etiam ut purus ultrices, pulvinar tellus quis, cursus massa. Mauris dignissim accumsan ex, at vestibulum lectus fermentum id.</div>
         <cc-input-text label="Name:"></cc-input-text>
@@ -44,7 +44,7 @@ export const defaultStory = makeStory(conf, {
         <cc-input-text label="Address:"></cc-input-text>
         <cc-button primary>ACTION!</cc-button>
       </cc-block-section>
-      <cc-block-section>
+      <cc-block-section slot="content">
         <div slot="title" class="danger">Danger section</div>
         <div slot="info">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque feugiat dui at leo porta dignissim. Etiam ut purus ultrices, pulvinar tellus quis, cursus massa. Mauris dignissim accumsan ex, at vestibulum lectus fermentum id.</div>
         <cc-button primary>THIRD!</cc-button>
@@ -58,14 +58,14 @@ export const infoWithEmptyColumn = makeStory(conf, {
   items: [
     {
       innerHTML: `
-      <div slot="title">This is my block</div>
-      <cc-block-section>
+      <div slot="header-title">This is my block</div>
+      <cc-block-section slot="content">
         <div slot="title">Subtitle Foo</div>
         <div slot="info"></div>
         <cc-input-text label="Name:"></cc-input-text>
         <cc-input-text label="Lastname:"></cc-input-text>
       </cc-block-section>
-      <cc-block-section>
+      <cc-block-section slot="content">
         <div slot="title">Subtitle Bar</div>
         <div slot="info"></div>
         <cc-input-text label="Name:"></cc-input-text>
@@ -82,13 +82,13 @@ export const infoWithNoInfoColumn = makeStory(conf, {
   items: [
     {
       innerHTML: `
-      <div slot="title">This is my block</div>
-      <cc-block-section>
+      <div slot="header-title">This is my block</div>
+      <cc-block-section slot="content">
         <div slot="title">Subtitle Foo</div>
         <cc-input-text label="Name:"></cc-input-text>
         <cc-input-text label="Lastname:"></cc-input-text>
       </cc-block-section>
-      <cc-block-section>
+      <cc-block-section slot="content">
         <div slot="title">Subtitle Bar</div>
         <cc-input-text label="Name:"></cc-input-text>
         <cc-input-text label="Lastname:"></cc-input-text>
