6caf0246a979b0aecd727a73cc35a34804b8953b
Florian Sanders <florian.sanders@clever-cloud.com>
2025-01-25 19:43:43 +0100

    refactor(tests): add WTR plugin for automated stories testing
    Fixes #963
    Fixes #854
    Fixes #731

---
+++ test/helpers/test-stories.types.d.ts
@@ -0,0 +1,27 @@
+import type { AnnotatedStoryFn } from '@storybook/csf';
+import type { StoryObj, WebComponentsRenderer } from '@storybook/web-components';
+
+export type StoryParams = StoryObj['parameters'];
+
+export type AccessibilityTestOptions = {
+  tests: {
+    accessibility: {
+      enable: boolean;
+      ignoredRules: Array<string>;
+    };
+  };
+};
+
+export type AnnotatedStoryFnClever = AnnotatedStoryFn<WebComponentsRenderer> & {
+  parameters: StoryParams & AccessibilityTestOptions;
+};
+
+export type RawStoriesModule = {
+  default: {
+    component: keyof HTMLElementTagNameMap;
+  };
+} & { [storyName: string]: AnnotatedStoryFnClever } & {};
+
+export type ArrayOfStoryFunctions = Array<{ storyName: string; storyFunction: AnnotatedStoryFnClever }>;
+
+export type ModuleEntryWithStoryFunction = [storyName: string, storyFunction: AnnotatedStoryFnClever];
