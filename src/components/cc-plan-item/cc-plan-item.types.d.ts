CleverCloud/design-system

feat(cc-plan-item): init component

src/components/cc-plan-item/cc-plan-item.types.d.ts
@@ -1,0 +1,11 @@
+import { BadgeIntent } from '../cc-badge/cc-badge.types.js';
+
+export interface PlanBadge {
+  content: string;
+  intent?: BadgeIntent;
+}
+
+export interface PlanDetails {
+  icon: object;
+  value: string;
+}

CleverCloud/clever-components

src/components/cc-plan-item/cc-plan-item.types.d.ts
@@ -1,0 +1,12 @@
+import { BadgeIntent } from '../cc-badge/cc-badge.types.js';
+import { IconModel } from '../common.types.js';
+
+export interface PlanBadge {
+  content: string;
+  intent?: BadgeIntent;
+}
+
+export interface PlanDetails {
+  icon: IconModel;
+  value: string;
+}