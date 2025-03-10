994d336262a7d238044afb4efdadd57f26686384
Pierre DE SOYRES <pierre.de-soyres@clever-cloud.com>
2025-03-10 16:00:18 +0100

    chore: fix JSDoc

--- src/components/cc-logs-app-access/cc-logs-app-access.smart.js
+++ src/components/cc-logs-app-access/cc-logs-app-access.smart.js
@@ -7,14 +7,14 @@ import './cc-logs-app-access.js';
 import { dateRangeSelectionToDateRange } from '../cc-logs-date-range-selector/date-range-selection.js';
 
 /**
- * @typedef {import('./cc-logs-app-access.js').CcLogsAppAccess} CcAccessLogs
+ * @typedef {import('./cc-logs-app-access.js').CcLogsAppAccess} CcLogsAppAccess
  * @typedef {import('../cc-logs/cc-logs.types.js').Log} Log
  * @typedef {import('../cc-logs-date-range-selector/cc-logs-date-range-selector.types.js').LogsDateRangeSelectionChangeEventData} LogsDateRangeSelectionChangeEventData
  * @typedef {import('../../lib/date/date-range.types.js').DateRange} DateRange
  * @typedef {import('../../lib/logs/logs-stream.types.js').LogsStreamState} LogsStreamState
  * @typedef {import('../../lib/send-to-api.types.js').ApiConfig} ApiConfig
- * @typedef {import('../../lib/smart/smart-component.types.js').OnContextUpdateArgs<CcAccessLogs>} OnContextUpdateArgs
- * @typedef {import('../../lib/smart/smart-component.types.js').UpdateComponentCallback<CcAccessLogs>} UpdateComponentCallback
+ * @typedef {import('../../lib/smart/smart-component.types.js').OnContextUpdateArgs<CcLogsAppAccess>} OnContextUpdateArgs
+ * @typedef {import('../../lib/smart/smart-component.types.js').UpdateComponentCallback<CcLogsAppAccess>} UpdateComponentCallback
  */
 
 defineSmartComponent({
@@ -83,7 +83,7 @@ class SmartController extends LogsStream {
    * @param {ApiConfig} _.apiConfig
    * @param {string} _.ownerId
    * @param {string} _.appId
-   * @param {CcAccessLogs} _.component
+   * @param {CcLogsAppAccess} _.component
    * @param {UpdateComponentCallback} _.updateComponent
    */
   constructor({ apiConfig, ownerId, appId, component, updateComponent }) {
