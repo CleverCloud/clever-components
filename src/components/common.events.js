a8d0896fdd2e9cd1fa1529ef7198d652183c0a59
Pierre DE SOYRES <pierre.de-soyres@clever-cloud.com>
2025-04-15 13:12:29 +0200

    refactor(cc-token-session-list)!: refactor events
    - rename `cc-token-session-list:revoke-all-session-tokens` event into `cc-tokens-session-revoke-all`
    - rename `cc-token-session-list:revoke-session-token` event into `cc-token-session-revoke`

--- src/components/common.events.js
+++ src/components/common.events.js
@@ -46,3 +46,70 @@ export class CcMultiSelectEvent extends CcEvent {
     super(CcMultiSelectEvent.TYPE, detail);
   }
 }
+
+/**
+ * Dispatched when an input content changed.
+ * @extends {CcEvent<T>}
+ * @template [T=string]
+ */
+export class CcInputEvent extends CcEvent {
+  static TYPE = 'cc-input';
+
+  /**
+   * @param {T} detail
+   */
+  constructor(detail) {
+    super(CcInputEvent.TYPE, detail);
+  }
+}
+
+/**
+ * Dispatched when an implicit form submission is requested.
+ * @extends {CcEvent}
+ */
+export class CcRequestSubmitEvent extends CcEvent {
+  static TYPE = 'cc-request-submit';
+
+  constructor() {
+    super(CcRequestSubmitEvent.TYPE);
+  }
+}
+
+/**
+ * Dispatched when a clickable element is clicked.
+ * @extends {CcEvent}
+ */
+export class CcClickEvent extends CcEvent {
+  static TYPE = 'cc-click';
+
+  constructor() {
+    super(CcClickEvent.TYPE);
+  }
+}
+
+/**
+ * Dispatched when revocation of all tokens is requested.
+ * @extends {CcEvent}
+ */
+export class CcTokensRevokeAllEvent extends CcEvent {
+  static TYPE = 'cc-tokens-revoke-all';
+
+  constructor() {
+    super(CcTokensRevokeAllEvent.TYPE);
+  }
+}
+
+/**
+ * Dispatched when a token revocation is requested.
+ * @extends {CcEvent<string>}
+ */
+export class CcTokenRevokeEvent extends CcEvent {
+  static TYPE = 'cc-token-revoke';
+
+  /**
+   * @param {string} detail
+   */
+  constructor(detail) {
+    super(CcTokenRevokeEvent.TYPE, detail);
+  }
+}
