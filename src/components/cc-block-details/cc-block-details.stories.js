f44ef7eac6a35b65f20dbb1ed22aac9d2540a670
clara-layus-cc <clara.layus@clever-cloud.com>
2025-04-29 15:00:41 +0200

    fix(cc-block-details): fix folder name and update style

--- src/components/cc-block-details/cc-block-details.stories.js
+++ src/components/cc-block-details/cc-block-details.stories.js
@@ -1,4 +1,5 @@
 import { makeStory } from '../../stories/lib/make-story.js';
+import { cliCommandsStyles } from '../../styles/cli-commands.js';
 import '../cc-block/cc-block.js';
 import './cc-block-details.js';
 
@@ -10,6 +11,7 @@ export default {
 
 const conf = {
   component: 'cc-block',
+  css: [cliCommandsStyles],
 };
 
 const ccBlockHtmlExample = `
@@ -17,12 +19,27 @@ const ccBlockHtmlExample = `
   <div slot="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque feugiat dui at leo porta dignissim. Etiam ut purus ultrices, pulvinar tellus quis, cursus massa. Mauris dignissim accumsan ex, at vestibulum lectus fermentum id. Quisque nec magna arcu. Quisque in metus sed erat sodales euismod eget id purus. Sed sagittis rhoncus mauris. Ut sit amet urna ac nunc semper porta. Nam ut felis eu velit luctus rutrum. Nam leo nisl, molestie a varius non, ullamcorper sit amet tortor. Donec in convallis ex. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent hendrerit venenatis erat, eu malesuada nulla viverra eu. Curabitur porta risus augue, non rutrum lectus hendrerit a.</div>
 `;
 
+const ccBlockDetailsContent = `
+  <p class="text">
+    Haec ad tenuem ideo aliquem ad haec tamquam te si.
+    Insidiarum perlato praedicto quo cogitabatur perlato cubiculi aurum diligens astute hoc opera ut mota et <a href="https://github.com/CleverCloud/clever-tools/blob/master/docs/setup-systems.md" title="documentation - Installing Clever Tools - new window">link</a>.
+  </p>
+  <dl>
+    <dt>Ad et securius ad perferens:</dt>
+    <dd><code>clever fake-command --fake-arg fake-app-id_a5a437c5-b9c7-41c2-97f3-fc6432a72ec0</code></dd>
+    <dt>Sollemni cum Caesare:</dt>
+    <dd><code>clever fake-command --fake-arg fake-app-id_a5a437c5-b9c7-41c2-97f3-fc6432a72ec0</code></dd>
+    <dt>Cautissimas et conserta habet:</dt>
+    <dd><code>clever fake-command --fake-arg fake-app-id_a5a437c5-b9c7-41c2-97f3-fc6432a72ec0</code></dd>
+  </dl>
+`;
+
 const defaultHtmlExample = `
   ${ccBlockHtmlExample}
   <cc-block-details slot="footer-left">
     <div slot="button-text">Command line</div>
     <a slot="link" href="">See documentation</a>
-    <div slot="content">Praesent tincidunt dapibus elit, sed posuere risus sodales quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer ultrices maximus ligula eu efficitur. Nulla luctus ipsum vitae dui gravida, sodales ornare dolor viverra. Vivamus pretium scelerisque ultricies. Nulla consectetur euismod euismod. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras dignissim id sapien non facilisis. Suspendisse vitae pretium orci. Nunc aliquam pellentesque nunc, at tincidunt nisi lobortis a. <a href="#">Fake link</a></div>
+    <div slot="content">${ccBlockDetailsContent}</div>
   </cc-block-details>
 `;
 
@@ -39,7 +56,7 @@ const openHtmlExample = `
   <cc-block-details is-open=true slot="footer-left">
     <div slot="button-text">Command line</div>
     <a slot="link" href="">See documentation</a>
-    <div slot="content">Praesent tincidunt dapibus elit, sed posuere risus sodales quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer ultrices maximus ligula eu efficitur. Nulla luctus ipsum vitae dui gravida, sodales ornare dolor viverra. Vivamus pretium scelerisque ultricies. Nulla consectetur euismod euismod. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras dignissim id sapien non facilisis. Suspendisse vitae pretium orci. Nunc aliquam pellentesque nunc, at tincidunt nisi lobortis a. <a href="#">Fake link</a></div>
+    <div slot="content">${ccBlockDetailsContent}</a></div>
   </cc-block-details>
 `;
 
@@ -55,7 +72,7 @@ const withoutLinkHtmlExample = `
   ${ccBlockHtmlExample}
   <cc-block-details slot="footer-left">
     <div slot="button-text">Command line</div>
-    <div slot="content">Praesent tincidunt dapibus elit, sed posuere risus sodales quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer ultrices maximus ligula eu efficitur. Nulla luctus ipsum vitae dui gravida, sodales ornare dolor viverra. Vivamus pretium scelerisque ultricies. Nulla consectetur euismod euismod. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras dignissim id sapien non facilisis. Suspendisse vitae pretium orci. Nunc aliquam pellentesque nunc, at tincidunt nisi lobortis a. <a href="#">Fake link</a></div>
+    <div slot="content">${ccBlockDetailsContent}</div>
   </cc-block-details>
 `;
 
