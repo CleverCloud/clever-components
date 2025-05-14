46be70d642bc8dc6a96adb2733b738ab954763a5
Helene Amouzou <helene.amouzou@clever-cloud.com>
2025-05-14 18:24:40 +0200

    feat(cc-oauth-consumer-form): init component

---
+++ src/components/cc-oauth-consumer-form/cc-oauth-consumer-form.smart-create.js
@@ -0,0 +1,92 @@
+// @ts-expect-error FIXME: remove when clever-client exports types
+import { create } from '@clevercloud/client/esm/api/v2/oauth-consumer.js';
+import { snakeCase } from '../../lib/change-case.js';
+import { i18n } from '../../lib/i18n/i18n.js';
+import { notifyError, notifySuccess } from '../../lib/notifications.js';
+import { sendToApi } from '../../lib/send-to-api.js';
+import { defineSmartComponent } from '../../lib/smart/define-smart-component.js';
+import '../cc-smart-container/cc-smart-container.js';
+import { CcOauthConsumerFormCreateEvent } from './cc-oauth-consumer-form.events.js';
+import './cc-oauth-consumer-form.js';
+
+/**
+ * @typedef {import('./cc-oauth-consumer-form.js').CcOauthConsumerForm} CcOauthConsumerForm
+ * @typedef {import('./cc-oauth-consumer-form.types.js').OauthConsumerWithoutKeyAndSecret} OauthConsumerWithoutKeyAndSecret
+ * @typedef {import('../cc-oauth-consumer-info/cc-oauth-consumer-info.types.js').OauthConsumer} OauthConsumer
+ * @typedef {import('../../lib/send-to-api.types.js').ApiConfig} ApiConfig
+ * @typedef {import('../../lib/smart/smart-component.types.js').OnContextUpdateArgs<CcOauthConsumerForm>} OnContextUpdateArgs
+ */
+
+defineSmartComponent({
+  selector: 'cc-oauth-consumer-form[smart-mode=create]',
+  params: {
+    apiConfig: { type: Object },
+    ownerId: { type: String },
+  },
+  /** @param {OnContextUpdateArgs} args */
+  onContextUpdate({ context, updateComponent, onEvent, component }) {
+    const { apiConfig, ownerId } = context;
+    const api = new Api(apiConfig, ownerId);
+
+    updateComponent('state', { type: 'idle-create' });
+
+    onEvent('cc-oauth-consumer-form-create', (data) => {
+      const oauthConsumerName = data.name;
+      updateComponent('state', (state) => {
+        state.type = 'creating';
+      });
+      api
+        .createOauthConsumer(data)
+        .then(() => {
+          notifySuccess(i18n('cc-oauth-consumer-form.create.success', { oauthConsumerName }));
+          component.resetOauthConsumerForm();
+          updateComponent('state', { type: 'idle-create' });
+          window.dispatchEvent(new CcOauthConsumerFormCreateEvent(data));
+        })
+        .catch(
+          /** @param {Error} error */
+          (error) => {
+            console.error(error);
+            notifyError(i18n('cc-oauth-consumer-form.create.error'));
+            updateComponent('state', (state) => {
+              state.type = 'idle-create';
+            });
+          },
+        );
+    });
+  },
+});
+
+class Api {
+  /**
+   * @param {ApiConfig} apiConfig
+   * @param {String} ownerId
+   */
+  constructor(apiConfig, ownerId) {
+    this._apiConfig = apiConfig;
+    this._ownerId = ownerId;
+  }
+
+  /**
+   * @param {OauthConsumerWithoutKeyAndSecret} data
+   * @return {*}
+   */
+  createOauthConsumer(data) {
+    const rights = Object.fromEntries(
+      Object.entries(data.rights).map(([name, isEnabled]) => {
+        const snakeCaseName = snakeCase(name);
+        return [snakeCaseName, isEnabled];
+      }),
+    );
+
+    const newOauthConsumer = {
+      name: data.name,
+      url: data.url,
+      baseUrl: data.baseUrl,
+      description: data.description,
+      picture: data.picture,
+      rights: rights,
+    };
+    return create({ id: this._ownerId }, newOauthConsumer).then(sendToApi({ apiConfig: this._apiConfig }));
+  }
+}
---
+++ src/components/cc-oauth-consumer-form/cc-oauth-consumer-form.smart-update.md
@@ -0,0 +1,61 @@
+---
+kind: '🛠 OAuth Consumer/<cc-oauth-consumer-form>'
+title: '💡 Smart (update)'
+---
+# 💡 Smart `<cc-oauth-consumer-form>`
+
+## ℹ️ Details
+
+<table>
+<tr><td><strong>Component    </strong> <td><a href="https://www.clever-cloud.com/doc/clever-components/?path=/story/🛠-oauth-consumer-cc-oauth-consumer-form--default-story"><code>&lt;cc-oauth-consumer-form&gt;</code></a>
+<tr><td><strong>Selector     </strong> <td><code>cc-oauth-consumer-form[smart-mode=update]</code>
+<tr><td><strong>Requires auth</strong> <td>Yes
+</table>
+
+## ⚙️ Params
+
+| Name        | Type         | Details                                                | Default |
+|-------------|--------------|--------------------------------------------------------|---------|
+| `apiConfig` | `ApiConfig`  | Object with API configuration (target host, tokens...) |         |
+| `ownerId`   | `string`     | UUID prefixed with orga_                               |         |
+| `key`       | `string`     | Random alphanumeric string                             |         |
+
+
+  ```ts
+interface ApiConfig {
+  API_HOST: string,
+  API_OAUTH_TOKEN: string,
+  API_OAUTH_TOKEN_SECRET: string,
+  OAUTH_CONSUMER_KEY: string,
+  OAUTH_CONSUMER_SECRET: string,
+}
+```
+
+## 🌐 API endpoints
+
+| Method   | URL                                       | Cache?  |
+|----------|-------------------------------------------|---------|
+| `GET`    | `/v2/organisations/${ownerId}/consumers/${key}`          | Default |
+| `REMOVE` | `/v2/organisations/${ownerId}/consumers/${key}`          | Default |
+| `UPDATE` | `/v2/organisations/${ownerId}/consumers/${key}`          | Default |
+
+
+
+## ⬇️️ Examples
+
+  ```html
+<cc-smart-container context='{
+    "apiConfig": {
+      API_HOST: "",
+      API_OAUTH_TOKEN: "",
+      API_OAUTH_TOKEN_SECRET: "",
+      OAUTH_CONSUMER_KEY: "",
+      OAUTH_CONSUMER_SECRET: "",
+    },
+    "ownerId": "",
+    "key": "",
+}'>
+  <cc-oauth-consumer-form></cc-oauth-consumer-form>
+</cc-smart-container>
+```
+
