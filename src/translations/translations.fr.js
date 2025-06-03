288b63795377fca5b137e7840f23ab4e1b4e071b
clara-layus-cc <clara.layus@clever-cloud.com>
2025-06-03 15:30:15 +0200

    feat(cc-env-var-form): add cc-block-details in the footer

--- src/translations/translations.fr.js
+++ src/translations/translations.fr.js
@@ -479,6 +479,21 @@ export const translations = {
   'cc-env-var-editor-simple.empty-data': `Il n'y a pas de variable.`,
   //#endregion
   //#region cc-env-var-form
+  'cc-env-var-form.cli.content': /** @param {{resourceId: string}} _ */ ({ resourceId }) =>
+    sanitize`
+      <p class="text">
+        Vous pouvez gérer les variables d'environnement directement depuis votre terminal en utilisant les commandes ci-dessous.
+        Si ce n'est pas déjà fait, installez rapidement les Clever Tools (notre CLI) à l'aide de notre <a href="https://github.com/CleverCloud/clever-tools/blob/master/docs/setup-systems.md" title="documentation - Installation des Clever Tools - nouvelle fenêtre">documentation</a>.
+      </p>
+      <dl>
+        <dt>Lister les variables d'environnement :</dt>
+        <dd><code>clever env --app ${resourceId}</code></dd>
+        <dt>Récupérer un fichier de variables d'environnement executable :</dt>
+        <dd><code>clever env --app ${resourceId} -F shell</code></dd>
+        <dt>Ajouter ou modifier une variable d'environnement :</dt>
+        <dd><code>clever env set VAR_NAME VAR_VALUE --app ${resourceId}</code></dd>
+      </dl>
+    `,
   'cc-env-var-form.description.config-provider': /** @param {{addonName: string}} _ */ ({ addonName }) =>
     sanitize`Configuration publiée pour les applications dépendantes. <a href="${generateDocsHref('/deploy/addon/config-provider/')}">En savoir plus</a><br>Ces seront injectées en tant que variables d'environnement dans les applications qui ont l'add-on <strong>${addonName}</strong> dans leurs services liés.<br>À chaque fois que vous mettez à jour les changements, toutes les applications dépendantes seront redémarrées automatiquement.`,
   'cc-env-var-form.description.env-var': /** @param {{appName: string}} _ */ ({ appName }) =>
