95fad16fbb7cd27da7208f805677e7ebc64333a2
clara-layus-cc <clara.layus@clever-cloud.com>
2025-06-12 17:27:37 +0200

    fix(cli-command): update cli-command style

--- src/styles/cli-commands.js
+++ src/styles/cli-commands.js
@@ -8,22 +8,4 @@ export const cliCommandsStyles = css`
   cc-block-details p {
     margin: 0;
   }
-  cc-block-details dt {
-    font-weight: bold;
-    margin: 1em 0 0.5em 0;
-  }
-  cc-block-details dd {
-    margin: 0;
-  }
-  cc-block-details dl {
-    margin: 0;
-  }
-  cc-block-details code {
-    padding: 0.5em 1em;
-    display: block;
-    border: 1px solid #dcdcdc;
-    background-color: var(--cc-color-bg-neutral);
-    border-radius: var(--cc-border-radius-default, 0.25em);
-    font-family: var(--cc-ff-monospace);
-  }
 `;
