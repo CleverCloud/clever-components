3ec5e365c6ce0ccfaffc62ad024179fb171a977d
Florian Sanders <florian.sanders@clever-cloud.com>
2025-06-12 16:51:51 +0200

    fix: new GH actions

--- web-test-runner.config.visual-regressions.js
+++ web-test-runner.config.visual-regressions.js
@@ -1,6 +1,5 @@
 import json from '@rollup/plugin-json';
 import { rollupAdapter } from '@web/dev-server-rollup';
-import { globSync } from 'tinyglobby';
 import { cemAnalyzerPlugin } from './wds/cem-analyzer-plugin.js';
 import { testVisualStoriesPlugin } from './wds/test-visual-stories-plugin.js';
 import { visualRegressionPluginWithConfig } from './wds/visual-regression-plugin.js';
@@ -10,13 +9,17 @@ import globalWtrConfig from './web-test-runner.config.js';
 export default {
   ...globalWtrConfig,
   groups: [
-    ...globSync(['src/components/**/*.stories.js']).map((path) => {
-      const groups = path.match(/^.*\/(?<fileName>.*)\.(?<fileType>.*)\.js/).groups;
-      return {
-        name: `${groups.fileType}:${groups.fileName}`,
-        files: path,
-      };
-    }),
+    // ...globSync(['src/components/**/*.stories.js']).map((path) => {
+    //   const groups = path.match(/^.*\/(?<fileName>.*)\.(?<fileType>.*)\.js/).groups;
+    //   return {
+    //     name: `${groups.fileType}:${groups.fileName}`,
+    //     files: path,
+    //   };
+    // }),
+    {
+      name: 'small',
+      files: 'src/components/cc-button/cc-button.stories.js',
+    },
   ],
   plugins: [
     cemAnalyzerPlugin,
