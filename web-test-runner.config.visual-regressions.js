ad5ff59a9e98435fd4ce93adaf43a1234e3940fe
Florian Sanders <florian.sanders@clever-cloud.com>
2025-06-19 19:16:47 +0200

    fix: branch name

--- web-test-runner.config.visual-regressions.js
+++ web-test-runner.config.visual-regressions.js
@@ -6,11 +6,10 @@ import { testVisualStoriesPlugin } from './wds/test-visual-stories-plugin.js';
 import { visualRegressionPluginWithConfig } from './wds/visual-regression-plugin.js';
 import { commonjsPluginWithConfig, esbuildBundlePluginWithConfig } from './wds/wds-common.js';
 import globalWtrConfig from './web-test-runner.config.js';
-import { myHtmlReporter } from './wtr-reporter-visual-regressions-html.js';
 
 export default {
   ...globalWtrConfig,
-  reporters: [...globalWtrConfig.reporters, myHtmlReporter()],
+  reporters: [...globalWtrConfig.reporters],
   groups: [
     ...getStoriesGroups(),
     // {
