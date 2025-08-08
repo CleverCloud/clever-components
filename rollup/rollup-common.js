1c67bbf296cb718e156cc6532d2f6bac3fcd7a81
Florian Sanders <florian.sanders@clever-cloud.com>
2025-08-08 11:46:26 +0200

    chore(build/minify): replace babel plugin with lit labs package
    As a temporary solution to be able to use container queries:
    - the babel plugin has been replaced because it is unmaintained,
    - the lit labs package relies on the same underlying packages
      `html-minifier` & `clean-css`,
    - force `clean-css` version to `5.3.3` because it handles `@container`
      rules properly.
    
    This is a temporary workaround:
    - we'll need to create a PR to bump `clean-css` in
      `@lit-labs/rollup-plugin-minify-html-literals` so that we don't have
      to override its version,
    - we still need to decide on whether we want to stop minifying or not.
    
    Fixes ##524
    Fixes #1387

--- rollup/rollup-common.js
+++ rollup/rollup-common.js
@@ -1,8 +1,8 @@
+import minifyTemplateLiterals from '@lit-labs/rollup-plugin-minify-html-literals';
 import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
 import CleanCSS from 'clean-css';
 import glob from 'glob';
 import path from 'path';
-import babel from 'rollup-plugin-babel';
 import clear from 'rollup-plugin-clear';
 import { terser } from 'rollup-plugin-terser';
 import visualizer from 'rollup-plugin-visualizer';
@@ -121,31 +121,24 @@ export function terserPlugin() {
   });
 }
 
-export function babelPlugin() {
-  return babel({
-    plugins: [
-      // Minify HTML inside lit-html and LitElement html`` templates
-      // Minify CSS inside LitElement css`` templates
-      [
-        'template-html-minifier',
-        {
-          modules: {
-            lit: ['html', { name: 'css', encapsulation: 'style' }],
-          },
-          htmlMinifier: {
-            caseSensitive: true,
-            collapseWhitespace: true,
-            removeAttributeQuotes: true,
-            removeComments: true,
-            removeRedundantAttributes: true,
-            // This clearly DOES NOT work well with template strings and lit-element
-            sortAttributes: false,
-            sortClassName: true,
-            minifyCSS: { level: 2 },
-          },
-        },
-      ],
-    ],
+export function minifyPlugin() {
+  return minifyTemplateLiterals({
+    failOnError: true,
+    // Trying to minify shoelace components for CDN release leads to a parser error
+    exclude: ['**/shoelace/**/*.js'],
+    options: {
+      minifyOptions: {
+        caseSensitive: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        // This clearly DOES NOT work well with template strings and lit-element
+        sortAttributes: false,
+        sortClassName: true,
+        minifyCSS: true,
+      },
+    },
   });
 }
 
