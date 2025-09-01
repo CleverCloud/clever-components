deed8994506493be9ecb1de4fb4cbcdd3c77e725
Florian Sanders <florian.sanders@clever-cloud.com>
2025-09-01 20:00:41 +0200

    --wip-- [skip ci]

---
+++ toto-3.js
@@ -0,0 +1,17 @@
+
+> @clevercloud/components@20.0.1 components:docs
+> cem analyze --outdir dist
+
+Declared properties only: [
+  { name: 'circle', type: 'boolean', jsdoc: undefined },
+  { name: 'icon', type: 'IconModel', jsdoc: undefined },
+  { name: 'iconA11yName', type: 'string', jsdoc: undefined },
+  {
+    name: 'intent',
+    type: '"neutral" | "info" | "success" | "warning" | "danger"',
+    jsdoc: undefined
+  },
+  { name: 'skeleton', type: 'boolean', jsdoc: undefined },
+  { name: 'weight', type: 'BadgeWeight', jsdoc: undefined }
+]
+[7:58:47 PM] @custom-elements-manifest/analyzer: Created new manifest.
