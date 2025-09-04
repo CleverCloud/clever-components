02e581d53d08e372820c9b293b5703fe5db77ee1
Pierre DE SOYRES <pierre.de-soyres@clever-cloud.com>
2025-09-04 16:55:25 +0200

    refactor(cc-env-var-linked-services)!: split smart component by type

--- src/components/cc-env-var-linked-services/cc-env-var-linked-services.smart-addon.js
+++ src/components/cc-env-var-linked-services/cc-env-var-linked-services.smart-addon.js
@@ -1,6 +1,5 @@
-// prettier-ignore
 // @ts-expect-error FIXME: remove when clever-client exports types
-import { get,getAllEnvVarsForAddons,getAllEnvVarsForDependencies } from '@clevercloud/client/esm/api/v2/application.js';
+import { get, getAllEnvVarsForAddons } from '@clevercloud/client/esm/api/v2/application.js';
 import { sendToApi } from '../../lib/send-to-api.js';
 import { defineSmartComponent } from '../../lib/smart/define-smart-component.js';
 import '../cc-smart-container/cc-smart-container.js';
@@ -18,10 +17,9 @@ import './cc-env-var-linked-services.js';
  */
 
 defineSmartComponent({
-  selector: 'cc-env-var-linked-services',
+  selector: 'cc-env-var-linked-services[type="addon"]',
   params: {
     apiConfig: { type: Object },
-    type: { type: String },
     ownerId: { type: String },
     appId: { type: String },
   },
@@ -29,10 +27,9 @@ defineSmartComponent({
    * @param {OnContextUpdateArgs} args
    */
   onContextUpdate({ context, updateComponent, signal }) {
-    const { apiConfig, type, ownerId, appId } = context;
+    const { apiConfig, ownerId, appId } = context;
 
-    updateComponent('state', { type: 'loading' });
-    updateComponent('type', type);
+    updateComponent('state', { type: 'loading', name: '' });
 
     const api = new Api(apiConfig, signal);
 
@@ -41,7 +38,7 @@ defineSmartComponent({
     });
 
     api
-      .fetchEnvVarLinkedService({ ownerId, appId, type })
+      .fetchEnvVarLinkedAddons({ ownerId, appId })
       .then((linkedServices) => {
         updateComponent('state', {
           type: 'loaded',
@@ -92,49 +89,9 @@ class Api {
    * @param {object} params
    * @param {string} params.ownerId
    * @param {string} params.appId
-   * @returns {Promise<LinkedService[]>}
-   */
-  async fetchEnvVarLinkedApps({ ownerId, appId }) {
-    const linkedApps = await getAllEnvVarsForDependencies({ id: ownerId, appId }).then(
-      sendToApi({ apiConfig: this.apiConfig, signal: this.signal }),
-    );
-
-    return linkedApps.map((/** @type {any} */ linkedApp) => {
-      return {
-        id: linkedApp.app_id,
-        name: linkedApp.app_name,
-        variables: linkedApp.env,
-      };
-    });
-  }
-
-  /**
-   * @param {object} params
-   * @param {string} params.ownerId
-   * @param {string} params.appId
    */
   async fetchAppName({ ownerId, appId }) {
     const app = await get({ id: ownerId, appId }).then(sendToApi({ apiConfig: this.apiConfig, signal: this.signal }));
     return app.name;
-  }
-
-  /**
-   * @param {object} params
-   * @param {string} params.ownerId
-   * @param {string} params.appId
-   * @param {EnvVarLinkedServicesType} params.type
-   */
-  async fetchEnvVarLinkedService({ ownerId, appId, type }) {
-    switch (type) {
-      case 'addon': {
-        return this.fetchEnvVarLinkedAddons({ ownerId, appId });
-      }
-      case 'app': {
-        return this.fetchEnvVarLinkedApps({ ownerId, appId });
-      }
-      default: {
-        return null;
-      }
-    }
   }
 }
