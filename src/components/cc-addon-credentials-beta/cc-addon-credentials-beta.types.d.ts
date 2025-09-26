71d6f2facc7c734a4b29615dffbfffdedbcc0117
Helene Amouzou <helene.amouzou@clever-cloud.com>
2025-09-26 15:54:51 +0200

    refactor: add-ons types
      - refactor `RawOperator` to be more generic
      - move commun add-ons types in `operators.types.d.ts`

--- src/components/cc-addon-credentials-beta/cc-addon-credentials-beta.types.d.ts
+++ src/components/cc-addon-credentials-beta/cc-addon-credentials-beta.types.d.ts
@@ -38,39 +38,3 @@ export interface RawAddon {
   creationDate: number;
   configKeys: string[];
 }
-
-export interface KeycloakOperatorInfo {
-  resourceId: string;
-  addonId: string;
-  name: string;
-  ownerId: string;
-  plan: string;
-  version: string;
-  javaVersion: string;
-  accessUrl: string;
-  availableVersions: string[];
-  resources: {
-    entrypoint: string;
-    fsbucketId: string;
-    pgsqlId: string;
-  };
-  features: {
-    networkGroup: null;
-  };
-  envVars: {
-    CC_KEYCLOAK_ADMIN: string;
-    CC_RUN_COMMAND: string;
-    CC_KEYCLOAK_DB_POOL_INITIAL_SIZE: string;
-    CC_METRICS_PROMETHEUS_PATH: string;
-    CC_JAVA_VERSION: string;
-    CC_PRE_RUN_HOOK: string;
-    CC_METRICS_PROMETHEUS_PORT: string;
-    CC_POST_BUILD_HOOK: string;
-    CC_KEYCLOAK_REALMS: string;
-    CC_KEYCLOAK_HOSTNAME: string;
-    CC_FS_BUCKET: string;
-    CC_KEYCLOAK_ADMIN_DEFAULT_PASSWORD: string;
-    CC_KEYCLOAK_VERSION: string;
-    CC_KEYCLOAK_DB_POOL_MAX_SIZE: string;
-  };
-}
