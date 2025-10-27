28918971f6b77fc3d72fbea2dcd6921c7e7e0bc7
Helene Amouzou <helene.amouzou@clever-cloud.com>
2025-10-27 14:58:37 +0100

    feat(cc-addon-info.smart-elastic): init

--- src/translations/translations.en.js
+++ src/translations/translations.en.js
@@ -300,6 +300,7 @@ export const translations = {
   'cc-addon-info.creation-date.heading': `Creation date`,
   'cc-addon-info.creation-date.human-friendly-date': /** @param {{ date: string | number }} _ */ ({ date }) =>
     formatDatetime(date),
+  'cc-addon-info.doc-link.elastic': `Elastic Stack - Documentation`,
   'cc-addon-info.doc-link.keycloak': `Keycloak - Documentation`,
   'cc-addon-info.doc-link.kubernetes': `Kubernetes - Documentation`,
   'cc-addon-info.doc-link.matomo': `Matomo - Documentation`,
