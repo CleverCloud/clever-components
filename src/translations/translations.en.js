a5690d989e217b54ceb1d86dcbdfb27a28f9ce43
Helene Amouzou <helene.amouzou@clever-cloud.com>
2025-10-27 18:23:19 +0100

    feat(cc-addon-info.smart-pulsar): init

--- src/translations/translations.en.js
+++ src/translations/translations.en.js
@@ -311,6 +311,7 @@ export const translations = {
   'cc-addon-info.doc-link.matomo': `Matomo - Documentation`,
   'cc-addon-info.doc-link.metabase': `Metabase - Documentation`,
   'cc-addon-info.doc-link.otoroshi': `Otoroshi - Documentation`,
+  'cc-addon-info.doc-link.pulsar': `Pulsar - Documentation`,
   'cc-addon-info.error': `Something went wrong while loading information`,
   'cc-addon-info.feature.connection-limit': `Connection limit`,
   'cc-addon-info.feature.cpu': `vCPUs`,
