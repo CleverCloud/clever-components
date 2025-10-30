69a7a45467e2cbd7015fb576cef7504a38928566
Robert Tran <robert.tran@clever-cloud.com>
2025-10-30 10:50:36 +0100

    fix(feedback): move range select event from common file to specific file

--- src/components/cc-range-selector/cc-range-selector.events.js
+++ src/components/cc-range-selector/cc-range-selector.events.js
@@ -1,6 +1,21 @@
 import { CcEvent } from '../../lib/events.js';
 
 /**
+ * Dispatched when a range selection changes.
+ * @extends {CcEvent<string[]>}
+ */
+export class CcRangeSelectEvent extends CcEvent {
+  static TYPE = 'cc-range-select';
+
+  /**
+   * @param {string[]} detail
+   */
+  constructor(detail) {
+    super(CcRangeSelectEvent.TYPE, detail);
+  }
+}
+
+/**
  * Dispatched when the custom option button is clicked.
  * The detail contains the current selection value (string for single mode, string array for range mode).
  * @extends {CcEvent<string|string[]>}
