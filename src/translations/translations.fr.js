6c53549ec634993ffe1304aa4df970f889ef5e14
Helene Amouzou <helene.amouzou@clever-cloud.com>
2025-11-12 17:32:41 +0100

    feat(cc-addon-info.smart-config-provider): init

--- src/translations/translations.fr.js
+++ src/translations/translations.fr.js
@@ -314,6 +314,7 @@ export const translations = {
   'cc-addon-info.creation-date.heading': `Date de création`,
   'cc-addon-info.creation-date.human-friendly-date': /** @param {{ date: string | number }} _ */ ({ date }) =>
     formatDatetime(date),
+  'cc-addon-info.doc-link.config-provider': `Configuration Provider - Documentation`,
   'cc-addon-info.doc-link.elastic': `Elastic Stack - Documentation`,
   'cc-addon-info.doc-link.keycloak': `Keycloak - Documentation`,
   'cc-addon-info.doc-link.kubernetes': `Kubernetes - Documentation`,
