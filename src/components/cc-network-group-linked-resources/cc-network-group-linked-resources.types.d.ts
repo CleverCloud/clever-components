d15ec012ea2c8c2b32871c1ff6ce48eb5799b268
Florian Sanders <florian.sanders@clever-cloud.com>
2025-12-12 19:23:56 +0100

    --wip-- [skip ci]

--- src/components/cc-network-group-linked-resources/cc-network-group-linked-resources.types.d.ts
+++ src/components/cc-network-group-linked-resources/cc-network-group-linked-resources.types.d.ts
@@ -1,3 +1,5 @@
+import { NetworkGroupPeer } from '../cc-network-group-peer-card/cc-network-group-peer-card.types.js';
+
 export type NetworkGroupLinkedResourcesState =
   | NetworkGroupLinkedResourcesStateLoaded
   | NetworkGroupLinkedResourcesStateUnlinking
@@ -32,13 +34,4 @@ export interface NetworkGroupMember {
   domainName: string;
   kind: 'APPLICATION' | 'EXTERNAL' | 'ADDON';
   peerList: NetworkGroupPeer[]; // could be an empty array
-}
-
-// TODO: if external, we need to provide a link to download the config
-export interface NetworkGroupPeer {
-  id: string;
-  label: string;
-  publicKey: string;
-  ip: string; // endpoint.ngTerm.host if CleverPeer | endpoint.ngIp if External peer TODO: CIDR
-  type: 'ExternalPeer' | 'CleverPeer';
 }
