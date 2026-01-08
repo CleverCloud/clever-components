85154eecea1739355e9419d8e9e502298eca7f06
Helene Amouzou <helene.amouzou@clever-cloud.com>
2026-01-08 14:48:15 +0100

    fix(cc-addon-info.smart-elastic): use `realId` in Granafa link

--- src/components/cc-addon-info/cc-addon-info.smart-elastic.js
+++ src/components/cc-addon-info/cc-addon-info.smart-elastic.js
@@ -201,7 +201,7 @@ class Api extends CcAddonInfoClient {
     const isKibanaEnabled = addonProvider.services.some((service) => service.name === 'kibana' && service.enabled);
     const grafanaAppLink =
       this._grafanaLink != null
-        ? await this._getGrafanaAppLink({ resourceId: addonProvider.app_id, signal: this._signal })
+        ? await this._getGrafanaAppLink({ resourceId: rawAddon.realId, signal: this._signal })
         : null;
 
     return { rawAddon, addonProvider, grafanaAppLink, isKibanaEnabled };
