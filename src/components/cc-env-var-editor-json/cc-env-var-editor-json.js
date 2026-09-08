2fa27ec6b2a14c199af900d774a8324775a79861
Pierre DE SOYRES <pierre.de-soyres@clever.cloud>
2026-09-08 14:56:03 +0200

    refactor(cc-env-var-editor-json): follow the client env var utils
    The parsing helpers moved from `esm/utils/env-vars.js` to `utils/environment-utils.js`. The parsing
    error type is `EnvVarParsingError`, from the client, instead of the local `EnvVarRawError`.
    
    Refs #1816

--- src/components/cc-env-var-editor-json/cc-env-var-editor-json.js
+++ src/components/cc-env-var-editor-json/cc-env-var-editor-json.js
@@ -1,4 +1,4 @@
-import { ERROR_TYPES, parseRawJson, toJson } from '@clevercloud/client/esm/utils/env-vars.js';
+import { ERROR_TYPES, parseRawJson, toJson } from '@clevercloud/client/utils/environment-utils.js';
 import { LitElement, css, html } from 'lit';
 import { i18n } from '../../translations/translation.js';
 import { CcEnvChangeEvent } from '../cc-env-var-form/cc-env-var-form.events.js';
@@ -16,7 +16,8 @@ const SKELETON_VARIABLES = [
 ];
 
 /**
- * @import { EnvVarEditorState, EnvVarParseError, EnvVar, EnvVarRawError } from '../common.types.js'
+ * @import { EnvVarParsingError } from '@clevercloud/client/utils/environment.types.js'
+ * @import { EnvVarEditorState, EnvVarParseError, EnvVar } from '../common.types.js'
  * @import { PropertyValues } from 'lit'
  */
 
@@ -60,7 +61,7 @@ export class CcEnvVarEditorJson extends LitElement {
     this._variablesAsJson = '';
   }
 
-  /** @param {EnvVarRawError[]} rawErrors */
+  /** @param {Array<EnvVarParsingError>} rawErrors */
   _setErrors(rawErrors) {
     this._errors = rawErrors.map(({ type, name }) => {
       if (type === ERROR_TYPES.INVALID_NAME) {
@@ -117,7 +118,6 @@ export class CcEnvVarEditorJson extends LitElement {
     }
 
     this._variablesAsJson = value;
-    /** @type {{ variables: EnvVar[], errors: EnvVarRawError[] }} */
     const { variables, errors } = parseRawJson(value, { mode: this.state.validationMode });
     this._setErrors(errors);
 
